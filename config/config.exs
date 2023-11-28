747d171ef08f50134dc221f1a64bf1e59bf48b1e
Tony Dang <tony@tonydang.blog>
2023-11-28 13:48:14 -0800

    rm esbuild config & dep

--- config/config.exs
+++ config/config.exs
@@ -36,14 +36,14 @@ config :live_view_svelte_offline_demo, LiveViewSvelteOfflineDemo.Mailer,
   adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
-config :esbuild,
-  version: "0.17.11",
-  default: [
-    args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
-    cd: Path.expand("../assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
+# config :esbuild,
+#   version: "0.17.11",
+#   default: [
+#     args:
+#       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+#     cd: Path.expand("../assets", __DIR__),
+#     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+#   ]
 
 # Configure tailwind (the version is required)
 config :tailwind,
--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule LiveViewSvelteOfflineDemo.MixProject do
       {:phoenix_live_view, "~> 0.20.1"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.2"},
-      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      # {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
