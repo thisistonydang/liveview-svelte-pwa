7c40b080b1f92a409b2d1e60fb930952bda73f00
Tony Dang <tony@tonydang.blog>
2023-12-03 10:23:13 -0800

    add daisyui

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
     },
   },
   plugins: [
+    require("daisyui"),
     require("@tailwindcss/forms"),
     // Allows prefixing tailwind classes with LiveView classes to add rules
     // only when LiveView classes are applied, for example:
