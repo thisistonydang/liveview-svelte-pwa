579ceb3225dd6f8f99de9a77f63a6f7274dc34db
Tony Dang <tony@tonydang.blog>
2023-12-04 13:41:37 -0800

    add auth routes to router

--- lib/live_view_svelte_offline_demo_web/router.ex
+++ lib/live_view_svelte_offline_demo_web/router.ex
@@ -1,6 +1,8 @@
 defmodule LiveViewSvelteOfflineDemoWeb.Router do
   use LiveViewSvelteOfflineDemoWeb, :router
 
+  import LiveViewSvelteOfflineDemoWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.Router do
     plug :put_root_layout, html: {LiveViewSvelteOfflineDemoWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -39,6 +42,44 @@ defmodule LiveViewSvelteOfflineDemoWeb.Router do
 
       live_dashboard "/dashboard", metrics: LiveViewSvelteOfflineDemoWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  ## Authentication routes
+
+  scope "/", LiveViewSvelteOfflineDemoWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{LiveViewSvelteOfflineDemoWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", LiveViewSvelteOfflineDemoWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{LiveViewSvelteOfflineDemoWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
+  end
+
+  scope "/", LiveViewSvelteOfflineDemoWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{LiveViewSvelteOfflineDemoWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
     end
   end
 end
