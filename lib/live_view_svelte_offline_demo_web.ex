c53ca277c9a18a8f05810c79773986653fc6554e
Tony Dang <tony@tonydang.blog>
2023-12-08 11:36:50 -0800

    allow serving sw.js from root

--- lib/live_view_svelte_offline_demo_web.ex
+++ lib/live_view_svelte_offline_demo_web.ex
@@ -17,7 +17,7 @@ defmodule LiveViewSvelteOfflineDemoWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt sw.js)
 
   def router do
     quote do
