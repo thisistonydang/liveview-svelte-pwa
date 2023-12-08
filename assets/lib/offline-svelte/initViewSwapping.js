e318dc19ef731b04bde7bc0d2c626ebbed0e24e1
Tony Dang <tony@tonydang.blog>
2023-12-07 16:22:02 -0800

    add initViewSwapping

---
+++ assets/lib/offline-svelte/initViewSwapping.js
@@ -0,0 +1,25 @@
+import { handleMessage } from "./handleMessage";
+import { requestOnlineStatus } from "./requestOnlineStatus";
+
+/**
+ * Initialize view swapping between live-view and dead-view.
+ *
+ * @param {{
+ *   liveSocket: * // TODO: Add type.
+ *   liveViewPath: string,
+ *   deadViewPath: string,
+ *   scrollPositionKey: string,
+ * }} options
+ */
+export function initViewSwapping({ liveSocket, ...options }) {
+  // Request online status from service worker on connectivity changes.
+  window.addEventListener("online", requestOnlineStatus);
+  window.addEventListener("offline", requestOnlineStatus);
+  liveSocket.socket.onClose(requestOnlineStatus);
+  liveSocket.socket.onError(requestOnlineStatus);
+
+  // Listen for messages from service worker.
+  navigator.serviceWorker?.addEventListener("message", (event) =>
+    handleMessage({ event, ...options }),
+  );
+}
