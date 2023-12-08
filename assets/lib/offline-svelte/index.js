134a40ad3047ebddbf0a701d016bd824cef8b07e
Tony Dang <tony@tonydang.blog>
2023-12-08 14:09:24 -0800

    export isClientStateRestored for public consumption

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -1,3 +1,4 @@
 export { initOfflineSvelte } from "./lib/initOfflineSvelte";
+export { isClientStateRestored } from "./lib/isClientStateRestored";
 export { isOnline } from "./lib/isOnline";
 export { requestOnlineStatus } from "./lib/requestOnlineStatus";
