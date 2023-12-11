f4ca57e8f559d3e29d9b33d53d7e0bf0f5066cb1
Tony Dang <tony@tonydang.blog>
2023-12-11 13:45:57 -0800

    require auth for /app & /fallback routes

--- lib/live_view_svelte_offline_demo_web/router.ex
+++ lib/live_view_svelte_offline_demo_web/router.ex
@@ -66,9 +66,12 @@ defmodule LiveViewSvelteOfflineDemoWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{LiveViewSvelteOfflineDemoWeb.UserAuth, :ensure_authenticated}] do
+      live "/app", AppLive
       live "/users/settings", UserSettingsLive, :edit
       live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
     end
+
+    get "/fallback", FallbackController, :index
   end
 
   scope "/", LiveViewSvelteOfflineDemoWeb do
