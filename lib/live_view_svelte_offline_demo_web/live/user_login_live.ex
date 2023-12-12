111fec883cfb828cdc4e8944755455e2224d8ef4
Tony Dang <tony@tonydang.blog>
2023-12-12 15:35:53 -0800

    update styling

--- lib/live_view_svelte_offline_demo_web/live/user_login_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_login_live.ex
@@ -3,34 +3,38 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserLoginLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Sign in to account
-        <:subtitle>
-          Don't have an account?
-          <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
-            Sign up
-          </.link>
-          for an account now.
-        </:subtitle>
-      </.header>
+    <div class="hero min-h-screen mx-auto max-w-md">
+      <div class="hero-content">
+        <div>
+          <.header class="text-center">
+            Sign in to account
+            <:subtitle>
+              Don't have an account?
+              <.link navigate={~p"/users/register"} class="font-semibold text-brand hover:underline">
+                Sign up
+              </.link>
+              for an account now.
+            </:subtitle>
+          </.header>
 
-      <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
+          <.simple_form for={@form} id="login_form" action={~p"/users/log_in"} phx-update="ignore">
+            <.input field={@form[:email]} type="email" label="Email" required />
+            <.input field={@form[:password]} type="password" label="Password" required />
 
-        <:actions>
-          <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
-          <.link href={~p"/users/reset_password"} class="text-sm font-semibold">
-            Forgot your password?
-          </.link>
-        </:actions>
-        <:actions>
-          <.button phx-disable-with="Signing in..." class="w-full">
-            Sign in <span aria-hidden="true">→</span>
-          </.button>
-        </:actions>
-      </.simple_form>
+            <:actions>
+              <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
+              <.link href={~p"/users/reset_password"} class="text-sm font-semibold">
+                Forgot your password?
+              </.link>
+            </:actions>
+            <:actions>
+              <.button phx-disable-with="Signing in..." class="w-full">
+                Sign in <span aria-hidden="true">→</span>
+              </.button>
+            </:actions>
+          </.simple_form>
+        </div>
+      </div>
     </div>
     """
   end
