199f66ea83efc41e1c1b332c867762514d5ce3ac
Tony Dang <tony@tonydang.blog>
2023-12-12 15:46:44 -0800

    update default button styling

--- lib/live_view_svelte_offline_demo_web/components/core_components.ex
+++ lib/live_view_svelte_offline_demo_web/components/core_components.ex
@@ -231,8 +231,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.CoreComponents do
     <button
       type={@type}
       class={[
-        "phx-submit-loading:opacity-75 rounded-lg bg-zinc-900 hover:bg-zinc-700 py-2 px-3",
-        "text-sm font-semibold leading-6 text-white active:text-white/80",
+        "phx-submit-loading:opacity-75 btn btn-accent",
         @class
       ]}
       {@rest}
