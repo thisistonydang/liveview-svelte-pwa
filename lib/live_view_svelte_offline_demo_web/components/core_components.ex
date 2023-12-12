512cef4c92ef150f47c9a2d3ce25e4135dcabe16
Tony Dang <tony@tonydang.blog>
2023-12-12 15:43:18 -0800

    update styling

--- lib/live_view_svelte_offline_demo_web/components/core_components.ex
+++ lib/live_view_svelte_offline_demo_web/components/core_components.ex
@@ -202,7 +202,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="mt-10 space-y-8 bg-white">
+      <div class="mt-10 space-y-8 p-4 border border-neutral rounded-lg">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
