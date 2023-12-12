ffb8e5963b7bdb5f22363c368b22de6acf83737a
Tony Dang <tony@tonydang.blog>
2023-12-12 15:38:24 -0800

    update styling

--- lib/live_view_svelte_offline_demo_web/live/user_registration_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_registration_live.ex
@@ -6,38 +6,44 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserRegistrationLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">
-        Register for an account
-        <:subtitle>
-          Already registered?
-          <.link navigate={~p"/users/log_in"} class="font-semibold text-brand hover:underline">
-            Sign in
-          </.link>
-          to your account now.
-        </:subtitle>
-      </.header>
+    <div class="hero min-h-screen mx-auto max-w-sm">
+      <div class="hero-content">
+        <div>
+          <.header class="text-center">
+            Register for an account
+            <:subtitle>
+              Already registered?
+              <.link navigate={~p"/users/log_in"} class="font-semibold text-brand hover:underline">
+                Sign in
+              </.link>
+              to your account now.
+            </:subtitle>
+          </.header>
 
-      <.simple_form
-        for={@form}
-        id="registration_form"
-        phx-submit="save"
-        phx-change="validate"
-        phx-trigger-action={@trigger_submit}
-        action={~p"/users/log_in?_action=registered"}
-        method="post"
-      >
-        <.error :if={@check_errors}>
-          Oops, something went wrong! Please check the errors below.
-        </.error>
+          <.simple_form
+            for={@form}
+            id="registration_form"
+            phx-submit="save"
+            phx-change="validate"
+            phx-trigger-action={@trigger_submit}
+            action={~p"/users/log_in?_action=registered"}
+            method="post"
+          >
+            <.error :if={@check_errors}>
+              Oops, something went wrong! Please check the errors below.
+            </.error>
 
-        <.input field={@form[:email]} type="email" label="Email" required />
-        <.input field={@form[:password]} type="password" label="Password" required />
+            <.input field={@form[:email]} type="email" label="Email" required />
+            <.input field={@form[:password]} type="password" label="Password" required />
 
-        <:actions>
-          <.button phx-disable-with="Creating account..." class="w-full">Create an account</.button>
-        </:actions>
-      </.simple_form>
+            <:actions>
+              <.button phx-disable-with="Creating account..." class="w-full">
+                Create an account
+              </.button>
+            </:actions>
+          </.simple_form>
+        </div>
+      </div>
     </div>
     """
   end
