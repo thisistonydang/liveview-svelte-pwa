eebb353c317b81b35ceb646a8f6e11440bcd4cf7
Tony Dang <tony@tonydang.blog>
2023-12-12 15:53:31 -0800

    update text color

--- lib/live_view_svelte_offline_demo_web/components/core_components.ex
+++ lib/live_view_svelte_offline_demo_web/components/core_components.ex
@@ -308,7 +308,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.CoreComponents do
 
     ~H"""
     <div phx-feedback-for={@name}>
-      <label class="flex items-center gap-4 text-sm leading-6 text-zinc-600">
+      <label class="flex items-center gap-4 text-sm leading-6">
         <input type="hidden" name={@name} value="false" />
         <input
           type="checkbox"
