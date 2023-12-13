cf2222f7ed3a4cb682ecb175583c95837b8209bf
Tony Dang <tony@tonydang.blog>
2023-12-12 16:49:19 -0800

    add ConnectionStatusBadge

---
+++ assets/svelte/ConnectionStatusBadge.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { isOnline } from "../lib/offline-svelte";
+</script>
+
+{#if $isOnline}
+  <div class="badge badge-accent">Connected</div>
+{:else}
+  <div class="badge badge-secondary">Disconnected</div>
+{/if}
