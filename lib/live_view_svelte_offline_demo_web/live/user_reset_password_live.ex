9dc7c0a451fa8d8abe0f8f0361d99c7e57e0a360
Tony Dang <tony@tonydang.blog>
2023-12-12 16:16:09 -0800

    update default styling

--- lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
@@ -5,35 +5,39 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserResetPasswordLive do
 
   def render(assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">Reset Password</.header>
+    <div class="hero min-h-screen mx-auto max-w-sm">
+      <div class="hero-content">
+        <div>
+          <.header class="text-center">Reset Password</.header>
 
-      <.simple_form
-        for={@form}
-        id="reset_password_form"
-        phx-submit="reset_password"
-        phx-change="validate"
-      >
-        <.error :if={@form.errors != []}>
-          Oops, something went wrong! Please check the errors below.
-        </.error>
+          <.simple_form
+            for={@form}
+            id="reset_password_form"
+            phx-submit="reset_password"
+            phx-change="validate"
+          >
+            <.error :if={@form.errors != []}>
+              Oops, something went wrong! Please check the errors below.
+            </.error>
 
-        <.input field={@form[:password]} type="password" label="New password" required />
-        <.input
-          field={@form[:password_confirmation]}
-          type="password"
-          label="Confirm new password"
-          required
-        />
-        <:actions>
-          <.button phx-disable-with="Resetting..." class="w-full">Reset Password</.button>
-        </:actions>
-      </.simple_form>
+            <.input field={@form[:password]} type="password" label="New password" required />
+            <.input
+              field={@form[:password_confirmation]}
+              type="password"
+              label="Confirm new password"
+              required
+            />
+            <:actions>
+              <.button phx-disable-with="Resetting..." class="w-full">Reset Password</.button>
+            </:actions>
+          </.simple_form>
 
-      <p class="text-center text-sm mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
-      </p>
+          <p class="text-center text-sm mt-4">
+            <.link href={~p"/users/register"}>Register</.link>
+            | <.link href={~p"/users/log_in"}>Log in</.link>
+          </p>
+        </div>
+      </div>
     </div>
     """
   end
