1517619b1d23e706d8dbcb4c802828d51974b760
Tony Dang <tony@tonydang.blog>
2023-12-13 14:33:46 -0800

    add App component

---
+++ assets/svelte/App.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { isClientStateRestored } from "../lib/offline-svelte";
+  import OfflineSvelte from "../lib/offline-svelte/OfflineSvelte.svelte";
+
+  import ClientOnlyStateManagement from "./ClientOnlyStateManagement.svelte";
+  import Header from "./Header.svelte";
+  import StateManagement from "./StateManagement.svelte";
+  import TodoApp from "./TodoApp.svelte";
+
+  export let live;
+  export let currentUserEmail;
+  export let serverState = {
+    meta: { synced: false },
+    timestamp: 0,
+    value: {
+      todo: [],
+      completed: [],
+    },
+  };
+</script>
+
+<StateManagement {live} {serverState} />
+<ClientOnlyStateManagement />
+<OfflineSvelte />
+
+{#if $isClientStateRestored}
+  <Header {currentUserEmail} />
+
+  <div class="max-w-2xl mx-auto px-2 md:p-0">
+    <TodoApp {live} />
+  </div>
+{:else}
+  <div class="hero min-h-screen">
+    <div class="hero-content">
+      <span class="loading loading-dots loading-lg"></span>
+    </div>
+  </div>
+{/if}
