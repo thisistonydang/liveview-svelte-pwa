47e9b830ca716b7ebdf8b5539ff210f218d7e0fe
Tony Dang <tony@tonydang.blog>
2023-12-12 18:25:52 -0800

    add Header component

---
+++ assets/svelte/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+  import AccountButton from "./AccountButton.svelte";
+  import ConnectionStatusBadge from "./ConnectionStatusBadge.svelte";
+
+  export let currentUserEmail;
+
+  let scrollY;
+</script>
+
+<svelte:window bind:scrollY />
+
+<div
+  class="sticky top-0 left-0 w-full py-1 z-10 bg-base-100 border-neutral"
+  class:border-b={scrollY > 0}
+>
+  <div class="max-w-2xl mx-auto px-2 md:p-0 flex justify-between items-center">
+    <ConnectionStatusBadge />
+    <AccountButton {currentUserEmail} />
+  </div>
+</div>
