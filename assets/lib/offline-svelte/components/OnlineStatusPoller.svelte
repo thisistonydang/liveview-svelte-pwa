356971a14c008da52c4ae8f344faceb3a6e37ec6
Tony Dang <tony@tonydang.blog>
2023-12-13 12:24:56 -0800

    add OnlineStatusPoller

---
+++ assets/lib/offline-svelte/components/OnlineStatusPoller.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+
+  import { pollIntervalId } from "../lib/pollIntervalId";
+  import { requestOnlineStatus } from "../lib/requestOnlineStatus";
+
+  export let fallbackPath;
+
+  onMount(() => {
+    const url = new URL(window.location.href);
+
+    if (url.pathname === fallbackPath) {
+      $pollIntervalId = setInterval(requestOnlineStatus, 1000);
+    }
+  });
+</script>
