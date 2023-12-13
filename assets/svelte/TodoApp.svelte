05252316e05be03e40efa2ae71b229ebe4d936b4
Tony Dang <tony@tonydang.blog>
2023-12-13 15:11:28 -0800

    use svg for delete button

--- assets/svelte/TodoApp.svelte
+++ assets/svelte/TodoApp.svelte
@@ -106,7 +106,16 @@
               <span class="line-through">{item.name}</span>
             </label>
             <button class="text-4xl" aria-label="Delete item." on:click={() => deleteItem(item)}>
-              &times;
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
             </button>
           </li>
         {/each}
