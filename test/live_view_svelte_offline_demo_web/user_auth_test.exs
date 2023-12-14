d75ba3e41a2d41d1b1702ff09d77928f659cb9da
Tony Dang <tony@tonydang.blog>
2023-12-14 15:54:13 -0800

    update auth test

--- test/live_view_svelte_offline_demo_web/user_auth_test.exs
+++ test/live_view_svelte_offline_demo_web/user_auth_test.exs
@@ -25,7 +25,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserAuthTest do
       conn = UserAuth.log_in_user(conn, user)
       assert token = get_session(conn, :user_token)
       assert get_session(conn, :live_socket_id) == "users_sessions:#{Base.url_encode64(token)}"
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/app"
       assert Accounts.get_user_by_session_token(token)
     end
 
