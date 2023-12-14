7641755fcccc1de07e78683fd0a32c1c64e0df59
Tony Dang <tony@tonydang.blog>
2023-12-14 15:52:08 -0800

    format

--- test/live_view_svelte_offline_demo_web/user_auth_test.exs
+++ test/live_view_svelte_offline_demo_web/user_auth_test.exs
@@ -11,7 +11,10 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserAuthTest do
   setup %{conn: conn} do
     conn =
       conn
-      |> Map.replace!(:secret_key_base, LiveViewSvelteOfflineDemoWeb.Endpoint.config(:secret_key_base))
+      |> Map.replace!(
+        :secret_key_base,
+        LiveViewSvelteOfflineDemoWeb.Endpoint.config(:secret_key_base)
+      )
       |> init_test_session(%{})
 
     %{user: user_fixture(), conn: conn}
