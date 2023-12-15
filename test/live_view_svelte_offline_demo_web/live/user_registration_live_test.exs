5545e478bacd4034f1cbea59fdc11db0cf6b6cea
Tony Dang <tony@tonydang.blog>
2023-12-14 16:09:20 -0800

    update auth test

--- test/live_view_svelte_offline_demo_web/live/user_registration_live_test.exs
+++ test/live_view_svelte_offline_demo_web/live/user_registration_live_test.exs
@@ -17,7 +17,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserRegistrationLiveTest do
         conn
         |> log_in_user(user_fixture())
         |> live(~p"/users/register")
-        |> follow_redirect(conn, "/")
+        |> follow_redirect(conn, "/app")
 
       assert {:ok, _conn} = result
     end
