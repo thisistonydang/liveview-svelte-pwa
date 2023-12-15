8e2568ab57989ae61c5151fe8264d1ba2e908b3b
Tony Dang <tony@tonydang.blog>
2023-12-14 16:05:42 -0800

    update auth test

--- test/live_view_svelte_offline_demo_web/live/user_login_live_test.exs
+++ test/live_view_svelte_offline_demo_web/live/user_login_live_test.exs
@@ -36,7 +36,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserLoginLiveTest do
 
       conn = submit_form(form, conn)
 
-      assert redirected_to(conn) == ~p"/"
+      assert redirected_to(conn) == ~p"/app"
     end
 
     test "redirects to login page with a flash error if there are no valid credentials", %{
