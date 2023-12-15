1a91f4fe094e858363da152d6a8afd6a7c7aebef
Tony Dang <tony@tonydang.blog>
2023-12-15 12:46:34 -0800

    update user_states migration

--- priv/repo/migrations/20231215203500_create_user_states.exs
+++ priv/repo/migrations/20231215203500_create_user_states.exs
@@ -3,12 +3,11 @@ defmodule LiveViewSvelteOfflineDemo.Repo.Migrations.CreateUserStates do
 
   def change do
     create table(:user_states) do
-      add :state, :map
-      add :user_id, references(:users, on_delete: :nothing)
-
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add :state, :map, null: false
       timestamps(type: :utc_datetime)
     end
 
-    create index(:user_states, [:user_id])
+    create unique_index(:user_states, [:user_id])
   end
 end
