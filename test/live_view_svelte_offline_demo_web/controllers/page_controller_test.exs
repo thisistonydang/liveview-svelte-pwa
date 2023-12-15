e3958ab049dd793235c4e2b2af69f4e804f8e192
Tony Dang <tony@tonydang.blog>
2023-12-14 16:03:12 -0800

    update homepage test

--- test/live_view_svelte_offline_demo_web/controllers/page_controller_test.exs
+++ test/live_view_svelte_offline_demo_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LiveViewSvelteOfflineDemoWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Offline-Enabled LiveView Demo"
   end
 end
