0598d09edba27e8610101863bd2d3e147dcf6eb7
Tony Dang <tony@tonydang.blog>
2023-12-15 08:55:35 -0800

    update auth test

--- test/live_view_svelte_offline_demo_web/live/user_login_live_test.exs
+++ test/live_view_svelte_offline_demo_web/live/user_login_live_test.exs
@@ -77,7 +77,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserLoginLiveTest do
 
       {:ok, conn} =
         lv
-        |> element(~s|main a:fl-contains("Forgot your password?")|)
+        |> element(~s|.hero a:fl-contains("Forgot your password?")|)
         |> render_click()
         |> follow_redirect(conn, ~p"/users/reset_password")
 
