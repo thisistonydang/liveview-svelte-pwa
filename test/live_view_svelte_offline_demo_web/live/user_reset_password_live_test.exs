204ff8c4081d21c0928dc4b1aa2cdfea4567014e
Tony Dang <tony@tonydang.blog>
2023-12-15 08:59:05 -0800

    update auth test

--- test/live_view_svelte_offline_demo_web/live/user_reset_password_live_test.exs
+++ test/live_view_svelte_offline_demo_web/live/user_reset_password_live_test.exs
@@ -93,11 +93,11 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserResetPasswordLiveTest do
 
       {:ok, conn} =
         lv
-        |> element(~s|main a:fl-contains("Log in")|)
+        |> element(~s|.hero a:fl-contains("Log in")|)
         |> render_click()
         |> follow_redirect(conn, ~p"/users/log_in")
 
-      assert conn.resp_body =~ "Log in"
+      assert conn.resp_body =~ "Sign in"
     end
 
     test "redirects to password reset page when the Register button is clicked", %{
