be9da016bd6a606313a6e33958796fc6459ef36c
Tony Dang <tony@tonydang.blog>
2023-12-15 16:06:47 -0800

    update seed file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,41 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias LiveViewSvelteOfflineDemo.Repo
+
+alias LiveViewSvelteOfflineDemo.Accounts
+alias LiveViewSvelteOfflineDemo.UserStates
+
+Repo.delete_all(Accounts.User)
+Repo.delete_all(UserStates.UserState)
+
+# Insert test user(s)
+Accounts.register_user(%{email: "test@example.com", password: "password1234"})
+
+# Insert test user_state(s)
+state = %{
+  "timestamp" => 1_701_807_051_829,
+  "value" => %{
+    "todo" => [
+      %{"id" => "1", "name" => "🍚 Oatmeal"},
+      %{"id" => "2", "name" => "🌱 Chia seeds"},
+      %{"id" => "3", "name" => "🥛 Milk"},
+      %{"id" => "4", "name" => "🍧 Yogurt"},
+      %{"id" => "5", "name" => "🧀 Cheese"},
+      %{"id" => "6", "name" => "🥜 Nuts"},
+      %{"id" => "7", "name" => "🪴 Hemp"}
+    ],
+    "completed" => [
+      %{"id" => "8", "name" => "🍫 Bars"},
+      %{"id" => "9", "name" => "🫙 Powder"},
+      %{"id" => "10", "name" => "🍌 Banana"},
+      %{"id" => "11", "name" => "◻️ Tofu"},
+      %{"id" => "12", "name" => "🍋 Lemon Juice"},
+      %{"id" => "13", "name" => "🍯 Honey"},
+      %{"id" => "14", "name" => "🧂 Salt"}
+    ]
+  }
+}
+
+UserStates.create_user_state(%{state: state, user_id: 1})
