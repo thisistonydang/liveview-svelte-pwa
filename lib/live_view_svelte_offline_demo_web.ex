75433dcf733b6f24975d62aa6e30462735156eda
Tony Dang <tony@tonydang.blog>
2023-12-19 15:05:20 -0800

    allow favicons to be served from root

--- lib/live_view_svelte_offline_demo_web.ex
+++ lib/live_view_svelte_offline_demo_web.ex
@@ -17,7 +17,23 @@ defmodule LiveViewSvelteOfflineDemoWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt sw.js)
+  def static_paths, do: ~w(
+    assets
+    fonts
+    images
+    favicon.ico
+    robots.txt
+    sw.js
+    android-chrome-192x192.png
+    android-chrome-512x512.png
+    apple-touch-icon.png
+    browserconfig.xml
+    favicon-16x16.png
+    favicon-32x32.png
+    mstile-150x150.png
+    safari-pinned-tab.svg
+    site.webmanifest
+  )
 
   def router do
     quote do
