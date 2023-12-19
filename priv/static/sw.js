925075e25061d64822dfb35b4bef02f2b980369d
Tony Dang <tony@tonydang.blog>
2023-12-19 15:15:20 -0800

    cache favicons

--- priv/static/sw.js
+++ priv/static/sw.js
@@ -1,9 +1,18 @@
-const CACHE_NAME = "2023-12-13v18";
+const CACHE_NAME = "2023-12-19v1";
 const DEBUG = false;
 const PUBLIC_ASSETS = [
   "/assets/app.css",
   "/assets/app.js",
   "/favicon.ico",
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png",
+  "/apple-touch-icon.png",
+  "/browserconfig.xml",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/mstile-150x150.png",
+  "/safari-pinned-tab.svg",
+  "/site.webmanifest",
 ]
 
 // Install _________________________________________________________________________________________
