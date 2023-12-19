d0ca66763f476eecca1e16797b666c0e1de898ed
Tony Dang <tony@tonydang.blog>
2023-12-19 10:23:50 -0800

    don't rely on fallbackPath to run poller

--- assets/lib/offline-svelte/components/OnlineStatusPoller.svelte
+++ assets/lib/offline-svelte/components/OnlineStatusPoller.svelte
@@ -4,13 +4,7 @@
   import { pollIntervalId } from "../lib/pollIntervalId";
   import { requestOnlineStatus } from "../lib/requestOnlineStatus";
 
-  export let liveViewPath;
-
   onMount(() => {
-    const url = new URL(window.location.href);
-
-    if (url.pathname !== liveViewPath) {
-      $pollIntervalId = setInterval(requestOnlineStatus, 1000);
-    }
+    $pollIntervalId = setInterval(requestOnlineStatus, 1000);
   });
 </script>
