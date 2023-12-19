bdd8b7061382e804fe087218109e91b1a2d4fb3e
Tony Dang <tony@tonydang.blog>
2023-12-19 12:47:57 -0800

    add SW update status stores

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -1,5 +1,7 @@
 export { initOfflineSvelte } from "./lib/initOfflineSvelte";
 export { isClientStateRestored } from "./lib/isClientStateRestored";
 export { isOnline } from "./lib/isOnline";
+export { isSWUpdateAvailable } from "./lib/isSWUpdateAvailable";
+export { isSWUpdateConfirmed } from "./lib/isSWUpdateConfirmed";
 export { requestAssetCaching } from "./lib/requestAssetCaching";
 export { requestOnlineStatus } from "./lib/requestOnlineStatus";
