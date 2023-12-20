1fbb812c849559e6c944d6231680ff22428069c7
Tony Dang <tony@tonydang.blog>
2023-12-20 12:56:09 -0800

    use initial_server_state when ssr-ing fallback

--- lib/live_view_svelte_offline_demo_web/controllers/fallback_html.ex
+++ lib/live_view_svelte_offline_demo_web/controllers/fallback_html.ex
@@ -1,9 +1,15 @@
 defmodule LiveViewSvelteOfflineDemoWeb.FallbackHTML do
   use LiveViewSvelteOfflineDemoWeb, :html
 
+  alias LiveViewSvelteOfflineDemo.UserStates
+
   def index(assigns) do
     ~H"""
-    <.App currentUserEmail={@current_user.email} isFallback={true} />
+    <.App
+      currentUserEmail={@current_user.email}
+      serverState={UserStates.initial_server_state()}
+      isFallback
+    />
     """
   end
 end
