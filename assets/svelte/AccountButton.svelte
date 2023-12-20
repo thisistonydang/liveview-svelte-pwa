4fc4d42a9fd5202f4cd0d725c970351b7de01bd1
Tony Dang <tony@tonydang.blog>
2023-12-20 07:28:08 -0800

    use details as account menu to avoid issues with js

--- assets/svelte/AccountButton.svelte
+++ assets/svelte/AccountButton.svelte
@@ -23,37 +23,32 @@
   }
 </script>
 
-<div class="relative" use:clickOutside={() => ($isAccountMenuOpened = false)}>
-  <button
-    class="my-1 btn btn-circle btn-neutral"
-    aria-label="Account Menu"
-    on:click={() => ($isAccountMenuOpened = !$isAccountMenuOpened)}
-  >
+<details
+  class="dropdown dropdown-end"
+  bind:open={$isAccountMenuOpened}
+  use:clickOutside={() => ($isAccountMenuOpened = false)}
+>
+  <summary class="my-1 btn btn-circle btn-neutral" aria-label="Account Menu">
     <UserSvgIcon />
-  </button>
+  </summary>
 
-  {#if $isAccountMenuOpened}
-    <div
-      class="menu absolute right-0 z-20 bg-base-200 border border-neutral rounded-box"
-      transition:fly={{ y: -5, duration: 100 }}
-    >
-      <p class="px-4 py-2 font-bold border-b border-neutral rounded-none mb-1.5">
-        {currentUserEmail}
-      </p>
+  <div class="dropdown-content menu bg-base-200 border border-neutral rounded-box">
+    <p class="px-4 py-2 font-bold border-b border-neutral rounded-none mb-1.5">
+      {currentUserEmail}
+    </p>
 
-      <ul class="w-56">
-        {#if $isOnline}
-          <li><a href="/users/settings">Settings</a></li>
-          <li><button on:click={logOutUser}>Log out</button></li>
-        {:else}
-          <li class="w-full tooltip tooltip-bottom" data-tip="You must be online to view settings.">
-            <button class="w-full hover:cursor-not-allowed" disabled>Settings</button>
-          </li>
-          <li class="w-full tooltip tooltip-bottom" data-tip="You must be online to log out.">
-            <button class="w-full hover:cursor-not-allowed" disabled>Log out</button>
-          </li>
-        {/if}
-      </ul>
-    </div>
-  {/if}
-</div>
+    <ul class="w-56">
+      {#if $isOnline}
+        <li><a href="/users/settings">Settings</a></li>
+        <li><button on:click={logOutUser}>Log out</button></li>
+      {:else}
+        <li class="w-full tooltip tooltip-bottom" data-tip="You must be online to view settings.">
+          <button class="w-full hover:cursor-not-allowed" disabled>Settings</button>
+        </li>
+        <li class="w-full tooltip tooltip-bottom" data-tip="You must be online to log out.">
+          <button class="w-full hover:cursor-not-allowed" disabled>Log out</button>
+        </li>
+      {/if}
+    </ul>
+  </div>
+</details>
