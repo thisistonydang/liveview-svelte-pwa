0bff0ecf8d879e16aed27b494d15ca377abec066
Tony Dang <tony@tonydang.blog>
2023-12-21 11:45:09 -0800

    add timeout to checkOnlineStatus

--- assets/lib/offline-svelte/lib/checkOnlineStatus.js
+++ assets/lib/offline-svelte/lib/checkOnlineStatus.js
@@ -2,8 +2,11 @@ import { connectionStatus } from "./connectionStatus";
 
 /**
  * Check if the client is online and set connectionStatus store.
+ *
+ * @param {Object} options
+ * @param {number} options.timeout - Default: 500
  */
-export async function checkOnlineStatus() {
+export async function checkOnlineStatus({ timeout } = { timeout: 500 }) {
   if (!window.navigator.onLine) {
     connectionStatus.set("Disconnected");
   }
@@ -11,9 +14,10 @@ export async function checkOnlineStatus() {
   try {
     const url = new URL(window.location.origin); // Avoid CORS errors with request to your own origin.
     url.searchParams.set("rand", Date.now().toString()); // Prevents cached responses.
-    const response = await fetch(url, { method: "HEAD" }); // HEAD request to avoid interception by service worker.
-    // TODO: Add timeout.
-    // signal: AbortSignal.timeout(500),
+    const response = await fetch(url, {
+      method: "HEAD", // HEAD request to avoid interception by service worker.
+      signal: AbortSignal.timeout(timeout), // Timeout to prevent excessive wait time.
+    });
 
     connectionStatus.set(response.ok ? "Connected" : "Disconnected");
   } catch {
