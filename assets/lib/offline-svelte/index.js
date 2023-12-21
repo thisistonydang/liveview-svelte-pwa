65ebc502734ec2450c09b482dda57eb7fdaafc10
Tony Dang <tony@tonydang.blog>
2023-12-20 20:10:25 -0800

    rm unused export

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -2,7 +2,6 @@ export { checkOnlineStatus } from "./lib/checkOnlineStatus";
 export { connectionStatus } from "./lib/connectionStatus";
 export { initOfflineSvelte } from "./lib/initOfflineSvelte";
 export { isClientStateRestored } from "./lib/isClientStateRestored";
-export { isOnline } from "./lib/isOnline";
 export { isSWUpdateAvailable } from "./lib/isSWUpdateAvailable";
 export { isSWUpdateConfirmed } from "./lib/isSWUpdateConfirmed";
 export { requestAssetCaching } from "./lib/requestAssetCaching";
