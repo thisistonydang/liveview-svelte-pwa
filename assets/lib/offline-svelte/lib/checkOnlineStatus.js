64fdae5a1a42565b90011e132b413b3388a17e86
Tony Dang <tony@tonydang.blog>
2023-12-20 20:15:03 -0800

    isOnline -> connectionStatus

--- assets/lib/offline-svelte/lib/checkOnlineStatus.js
+++ assets/lib/offline-svelte/lib/checkOnlineStatus.js
@@ -1,20 +1,22 @@
-import { isOnline } from "./isOnline";
+import { connectionStatus } from "./connectionStatus";
 
 /**
  * Check if the client is online and set isOnline store.
  */
 export async function checkOnlineStatus() {
   if (!window.navigator.onLine) {
-    isOnline.set(false);
+    connectionStatus.set("Disconnected");
   }
 
   try {
     const url = new URL(window.location.origin); // Avoid CORS errors with request to your own origin.
     url.searchParams.set("rand", Date.now().toString()); // Prevents cached responses.
     const response = await fetch(url, { method: "HEAD" }); // HEAD request to avoid interception by service worker.
+    // TODO: Add timeout.
+    // signal: AbortSignal.timeout(500),
 
-    isOnline.set(response.ok);
+    connectionStatus.set(response.ok ? "Connected" : "Disconnected");
   } catch {
-    isOnline.set(false);
+    connectionStatus.set("Disconnected");
   }
 }
