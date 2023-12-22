5612f2b7fa174f0ec51a1b2709dde6f61ccb6588
Tony Dang <tony@tonydang.blog>
2023-12-22 14:15:01 -0800

    extract root_html custom component

--- lib/live_view_svelte_offline_demo_web/components/custom_components.ex
+++ lib/live_view_svelte_offline_demo_web/components/custom_components.ex
@@ -1,3 +1,45 @@
 defmodule LiveViewSvelteOfflineDemoWeb.CustomComponents do
   use Phoenix.Component
+
+  # Allow use of sigil_p/2
+  use Phoenix.VerifiedRoutes,
+    endpoint: LiveViewSvelteOfflineDemoWeb.Endpoint,
+    router: LiveViewSvelteOfflineDemoWeb.Router,
+    statics: LiveViewSvelteOfflineDemoWeb.static_paths()
+
+  slot :inner_block, required: true
+
+  def root_html(assigns) do
+    ~H"""
+    <!DOCTYPE html>
+    <html lang="en" class="[scrollbar-gutter:stable]">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00ccb7" />
+        <meta name="apple-mobile-web-app-title" content="ToDo" />
+        <meta name="application-name" content="ToDo" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#1d232a" />
+        <!-- End Favicons -->
+        <meta name="csrf-token" content={Phoenix.Controller.get_csrf_token()} />
+        <.live_title suffix=" · Offline-Enabled LiveView Svelte Demo">
+          ToDo
+        </.live_title>
+        <meta name="description" content="Offline-Enabled LiveView Svelte Demo App" />
+        <link phx-track-static rel="stylesheet" href={~p"/assets/app.css"} />
+        <script defer phx-track-static type="text/javascript" src={~p"/assets/app.js"}>
+        </script>
+      </head>
+      <body class="antialiased">
+        <%= render_slot(@inner_block) %>
+      </body>
+    </html>
+    """
+  end
 end
