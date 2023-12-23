15830555c590dce92a224e548ed44d3c9707710c
Tony Dang <tony@tonydang.blog>
2023-12-23 14:20:08 -0800

    Add ErrorLayout

---
+++ assets/svelte/ErrorLayout.svelte
@@ -0,0 +1,36 @@
+<script>
+  import FrownSvgIcon from "./FrownSvgIcon.svelte";
+
+  export let live;
+  live;
+
+  export let title;
+  export let subtitle = undefined;
+  export let linkTo;
+  export let linkText;
+</script>
+
+<div class="hero min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <div class="mx-auto w-40 h-40">
+        <FrownSvgIcon />
+      </div>
+      <h1 class="text-xl font-bold my-3">{title}</h1>
+
+      {#if subtitle}
+        <p class="my-2">{subtitle}</p>
+      {/if}
+
+      <a
+        class="btn btn-accent my-2"
+        href={linkTo}
+        on:click|preventDefault={() => {
+          window.location.replace(linkTo);
+        }}
+      >
+        {linkText}
+      </a>
+    </div>
+  </div>
+</div>
