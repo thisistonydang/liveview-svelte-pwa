3139ee9d9ea6092402f6981be0a37cfffa4bbd22
Tony Dang <tony@tonydang.blog>
2023-12-23 13:34:08 -0800

    add sw.config.js

---
+++ priv/static/sw.config.js
@@ -0,0 +1,23 @@
+export default {
+  cacheName: "2023-12-20v20",
+  debug: false,
+  privateAssets: [
+    "/app", 
+    "/fallback"
+  ],
+  publicAssets: [
+    "/assets/app.css",
+    "/assets/app.js",
+    "/favicon.ico",
+    "/android-chrome-192x192.png",
+    "/android-chrome-512x512.png",
+    "/apple-touch-icon.png",
+    "/browserconfig.xml",
+    "/favicon-16x16.png",
+    "/favicon-32x32.png",
+    "/mstile-150x150.png",
+    "/safari-pinned-tab.svg",
+    "/site.webmanifest",
+    "/offline"
+  ]
+}
