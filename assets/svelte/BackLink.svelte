8fd805e9db0f08970ddda71d36fe1d9be74c5d73
Tony Dang <tony@tonydang.blog>
2023-12-23 14:51:02 -0800

    add BackLink

---
+++ assets/svelte/BackLink.svelte
@@ -0,0 +1,25 @@
+<script>
+  import ArrowLeftSvgIcon from "./ArrowLeftSvgIcon.svelte";
+
+  export let live;
+  live;
+
+  export let linkTo;
+  export let linkText;
+</script>
+
+<div class="mt-4">
+  <a
+    class="link link-accent no-underline text-sm font-semibold flex items-center gap-1"
+    href={linkTo}
+    on:click|preventDefault={() => {
+      window.location.replace(linkTo);
+    }}
+  >
+    <span class="h-3 w-3">
+      <ArrowLeftSvgIcon />
+    </span>
+
+    {linkText}
+  </a>
+</div>
