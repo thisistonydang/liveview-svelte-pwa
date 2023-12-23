b5fcb9c832ee7ff2f1ab3b003a8509bcfd7b18a9
Tony Dang <tony@tonydang.blog>
2023-12-23 13:01:10 -0800

    change sw to ES module and require updating imports via network

--- assets/lib/offline-svelte/lib/registerServiceWorker.js
+++ assets/lib/offline-svelte/lib/registerServiceWorker.js
@@ -9,7 +9,10 @@ export function registerServiceWorker(serviceWorkerPath) {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", async () => {
       try {
-        const registration = await navigator.serviceWorker.register(serviceWorkerPath);
+        const registration = await navigator.serviceWorker.register(serviceWorkerPath, {
+          type: "module", // Allow ES module imports in sw.
+          updateViaCache: "none", // Always update sw imports from network to prevent caching issues.
+        });
         DEBUG && console.log("[Service Worker] Registered.", registration);
       } catch (error) {
         console.error("[Service Worker] Registration Failed.", error);
