afd0e10dfb234cd2806b8dea0e5af66397f3c835
Tony Dang <tony@tonydang.blog>
2023-12-26 09:39:32 -0800

    don't export requestOnlineStatus

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -6,4 +6,3 @@ export { isSWUpdateAvailable } from "./lib/isSWUpdateAvailable";
 export { isSWUpdateConfirmed } from "./lib/isSWUpdateConfirmed";
 export { requestAssetCaching } from "./lib/requestAssetCaching";
 export { requestAssetDeletion } from "./lib/requestAssetDeletion";
-export { requestOnlineStatus } from "./lib/requestOnlineStatus";
