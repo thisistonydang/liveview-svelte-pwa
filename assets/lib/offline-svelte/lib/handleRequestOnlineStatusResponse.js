e25711bd68338f5d217a0d91b63a9e4543ea2db0
Tony Dang <tony@tonydang.blog>
2023-12-26 08:32:42 -0800

    rm pollIntervalId

--- assets/lib/offline-svelte/lib/handleRequestOnlineStatusResponse.js
+++ assets/lib/offline-svelte/lib/handleRequestOnlineStatusResponse.js
@@ -2,7 +2,6 @@ import { get } from "svelte/store";
 
 import { isSvelteMounted } from "./isSvelteMounted";
 import { connectionStatus } from "./connectionStatus";
-import { pollIntervalId } from "./pollIntervalId";
 
 /**
  * Handle "request_online_status" response from service worker.
@@ -25,12 +24,6 @@ export function handleRequestOnlineStatusResponse({ event, liveViewPath, fallbac
     get(connectionStatus) === "Connected" &&
     svelteMounted
   ) {
-    // Clear polling interval.
-    const intervalId = get(pollIntervalId);
-    if (intervalId) {
-      clearInterval(intervalId);
-    }
-
     window.location.replace(liveViewPath);
   }
 }
