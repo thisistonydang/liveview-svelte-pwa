5e670aa0df94e516fa60fcb704e40535a903069f
Tony Dang <tony@tonydang.blog>
2023-12-27 12:39:05 -0800

    use no socket flash msg layout

--- lib/live_view_svelte_offline_demo_web/controllers/app_controller.ex
+++ lib/live_view_svelte_offline_demo_web/controllers/app_controller.ex
@@ -2,6 +2,9 @@ defmodule LiveViewSvelteOfflineDemoWeb.AppController do
   use LiveViewSvelteOfflineDemoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+    render(conn, :index,
+      hide_socket_flash_messages: true,
+      layout: {LiveViewSvelteOfflineDemoWeb.Layouts, :spa}
+    )
   end
 end
