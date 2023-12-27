9c01c6ae2989f9b404d3a54b4393937d8c1d7be2
Tony Dang <tony@tonydang.blog>
2023-12-27 10:43:53 -0800

    add LiveViewSocket component

---
+++ assets/svelte/LiveViewSocket.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  import { isSocketMounted, liveView, serverState, sessionCount } from "../stores/liveViewSocket";
+
+  // Store 'live' object from live_svelte in a Svelte store.
+  export let live;
+  $: $liveView = live;
+
+  // Receive socket assigns from LiveView.
+  export let server_state;
+  export let session_count;
+
+  // Assign socket assigns to reactive stores.
+  $: $serverState = server_state;
+  $: $sessionCount = session_count;
+
+  onMount(() => {
+    $isSocketMounted = true;
+  });
+</script>
