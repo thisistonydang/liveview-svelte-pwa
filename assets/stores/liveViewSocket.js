ef03e66a27623987153c21d7b01a06f6aec7c109
Tony Dang <tony@tonydang.blog>
2023-12-27 11:22:16 -0800

    rm default serverState store value

--- assets/stores/liveViewSocket.js
+++ assets/stores/liveViewSocket.js
@@ -6,16 +6,6 @@ export let isSocketMounted = writable(false);
 // Store containing 'live' object from live_svelte.
 export let liveView = writable();
 
-// Socket Assigns from LiveView ____________________________________________________________________
-
-// Initial default state is required so that syncServerToClient can run when
-// offline. This is necessary because syncServerToClient is what sets the
-// initial states for the $todoItems, $completedItems, and $syncState stores.
-export let serverState = writable({
-  meta: { synced: true },
-  timestamp: 0,
-  value: { todo: [], completed: [] },
-});
-
-// Count of active sessions a user has.
+// Socket assigns from LiveView
+export let serverState = writable();
 export let sessionCount = writable();
