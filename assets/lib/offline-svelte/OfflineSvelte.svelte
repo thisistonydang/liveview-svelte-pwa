3aebbc07459c49b793fd2591df969762ac3e6931
Tony Dang <tony@tonydang.blog>
2023-12-27 15:21:14 -0800

    add ServiceWorkerMessageHandler

--- assets/lib/offline-svelte/OfflineSvelte.svelte
+++ assets/lib/offline-svelte/OfflineSvelte.svelte
@@ -1,9 +1,11 @@
 <script>
   import ScrollPositionRestorer from "./components/ScrollPositionRestorer.svelte";
+  import ServiceWorkerMessageHandler from "./components/ServiceWorkerMessageHandler.svelte";
   import ServiceWorkerUpdater from "./components/ServiceWorkerUpdater.svelte";
 
   export let scrollPositionKey = "scrollPosition";
 </script>
 
 <ScrollPositionRestorer {scrollPositionKey} />
+<ServiceWorkerMessageHandler />
 <ServiceWorkerUpdater />
---
+++ assets/lib/offline-svelte/components/ServiceWorkerMessageHandler.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    navigator.serviceWorker?.addEventListener("message", (event) => {
+      switch (event.data.type) {
+        case "request_skip_waiting":
+          window.location.reload();
+          break;
+
+        default:
+          console.error("Unknown message type received from service worker.", event.data);
+      }
+    });
+  });
+</script>
