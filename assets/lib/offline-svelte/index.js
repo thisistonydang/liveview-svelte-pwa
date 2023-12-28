71551ed66e84b84eb8b457fd457c0a234e2dc89b
Tony Dang <tony@tonydang.blog>
2023-12-28 11:03:01 -0800

    export registerServiceWorker from offline-svelte

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -3,5 +3,6 @@ export { connectionStatus } from "./lib/connectionStatus";
 export { isClientStateRestored } from "./lib/isClientStateRestored";
 export { isSWUpdateAvailable } from "./lib/isSWUpdateAvailable";
 export { isSWUpdateConfirmed } from "./lib/isSWUpdateConfirmed";
+export { registerServiceWorker } from "./lib/registerServiceWorker";
 export { requestAssetCaching } from "./lib/requestAssetCaching";
 export { requestAssetDeletion } from "./lib/requestAssetDeletion";
