7ec6d4dfd74f4cd2f6c662cf896ae1f7a8214b7e
Tony Dang <tony@tonydang.blog>
2023-12-27 16:10:16 -0800

    add DataClearer

---
+++ assets/svelte/DataClearer.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from "svelte";
+  import { requestAssetDeletion } from "../lib/offline-svelte";
+  import config from "../../priv/static/sw.config.js";
+
+  export let live;
+  live;
+
+  onMount(() => {
+    localStorage.clear();
+    requestAssetDeletion(config.privateAssets);
+  });
+</script>
