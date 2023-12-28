7bcecd40cf7102ccdb268ab76d7dd287e5b1407c
Tony Dang <tony@tonydang.blog>
2023-12-28 14:54:21 -0800

    rm sessionBadge fade

--- assets/svelte/SessionsBadge.svelte
+++ assets/svelte/SessionsBadge.svelte
@@ -1,10 +1,9 @@
 <script>
-  import { fade } from "svelte/transition";
   import { sessionCount } from "../stores/liveViewSocket";
 </script>
 
 {#if $sessionCount}
-  <div transition:fade class="badge badge-xs transition-none p-2">
+  <div class="badge badge-xs transition-none p-2">
     {$sessionCount} Session{$sessionCount > 1 ? "s" : ""}
   </div>
 {/if}
