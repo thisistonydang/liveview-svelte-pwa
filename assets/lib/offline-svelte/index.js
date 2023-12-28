31363714589db658c403aaf061ceece46d2d29f8
Tony Dang <tony@tonydang.blog>
2023-12-28 10:59:29 -0800

    rm initOfflineSvelte

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -1,6 +1,5 @@
 export { checkOnlineStatus } from "./lib/checkOnlineStatus";
 export { connectionStatus } from "./lib/connectionStatus";
-export { initOfflineSvelte } from "./lib/initOfflineSvelte";
 export { isClientStateRestored } from "./lib/isClientStateRestored";
 export { isSWUpdateAvailable } from "./lib/isSWUpdateAvailable";
 export { isSWUpdateConfirmed } from "./lib/isSWUpdateConfirmed";
