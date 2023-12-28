b00ab3c937544db71dff675011eeea8797a70583
Tony Dang <tony@tonydang.blog>
2023-12-28 11:04:40 -0800

    refactor connectionStatus -> isConnected boolean

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -1,6 +1,5 @@
-export { checkOnlineStatus } from "./lib/checkOnlineStatus";
-export { connectionStatus } from "./lib/connectionStatus";
 export { isClientStateRestored } from "./lib/isClientStateRestored";
+export { isConnected } from "./lib/isConnected";
 export { isSWUpdateAvailable } from "./lib/isSWUpdateAvailable";
 export { isSWUpdateConfirmed } from "./lib/isSWUpdateConfirmed";
 export { registerServiceWorker } from "./lib/registerServiceWorker";
--- assets/lib/offline-svelte/lib/connectionStatus.js
+++
@@ -1,3 +0,0 @@
-import { writable } from "svelte/store";
-
-export const connectionStatus = writable("Connected"); // "Connected" | "Checking" | "Disconnected"
