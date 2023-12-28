942c66cfd8ca8066f9ed4f9bb676b92adcfc2a5e
Tony Dang <tony@tonydang.blog>
2023-12-28 09:22:59 -0800

    add isConnected helper

---
+++ assets/lib/offline-svelte/lib/isConnected.js
@@ -0,0 +1,31 @@
+/**
+ * Check if the client can connect to the origin server.
+ *
+ * @param {Object} options
+ * @param {string} options.pathname - URL path to try to send fetch request. Default: "/"
+ * @param {number} options.timeout - Default: 500 milliseconds
+ *
+ * @returns {Promise<boolean>}
+ */
+export async function isConnected({ pathname, timeout } = { pathname: "/", timeout: 500 }) {
+  if (!window.navigator.onLine) {
+    return false;
+  }
+
+  try {
+    // Avoid CORS errors with request to own origin.
+    const url = new URL(`${window.location.origin}${pathname}`);
+
+    // Add random query parameter to prevent cached responses.
+    url.searchParams.set("rand", Date.now().toString());
+
+    const response = await fetch(url, {
+      method: "HEAD", // HEAD request to speed up response and avoid interception by service worker.
+      signal: AbortSignal.timeout(timeout), // Timeout to prevent excessive wait time.
+    });
+
+    return response.ok;
+  } catch {
+    return false;
+  }
+}
