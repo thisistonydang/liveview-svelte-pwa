e06271cd693c09493d5716fcd53b26f9626abba2
Tony Dang <tony@tonydang.blog>
2023-12-29 09:10:25 -0800

    track newTodo state in localStorage

--- assets/svelte/ClientOnlyStateManagement.svelte
+++ assets/svelte/ClientOnlyStateManagement.svelte
@@ -1,22 +1,28 @@
 <script>
   import { onMount } from "svelte";
   import { isClientStateRestored } from "../lib/offline-svelte";
-  import { isAccountMenuOpened, isCompletedOpened, isTodoOpened } from "../stores/clientOnlyState";
+  import {
+    isAccountMenuOpened,
+    isCompletedOpened,
+    isTodoOpened,
+    newTodo,
+  } from "../stores/clientOnlyState";
 
   /**
    * Get parsed value from localStorage.
    *
    * @param {string} key - Key to get from localStorage.
+   * @param {string} type - Type of value.
    * @param {boolean} defaultValue - Default value to return if value is not found in localStorage.
    */
-  function getParseValue(key, defaultValue) {
+  function getParseValue(key, type, defaultValue) {
     const value = localStorage.getItem(key);
 
     if (!value) return defaultValue;
 
     try {
       const parsedValue = JSON.parse(value);
-      return typeof parsedValue === "boolean" ? parsedValue : defaultValue;
+      return typeof parsedValue === type ? parsedValue : defaultValue;
     } catch {
       return defaultValue;
     }
@@ -24,9 +30,10 @@
 
   onMount(() => {
     // Sync client state stores with localStorage on startup.
-    $isAccountMenuOpened = getParseValue("isAccountMenuOpened", false);
-    $isTodoOpened = getParseValue("isTodoOpened", true);
-    $isCompletedOpened = getParseValue("isCompletedOpened", false);
+    $isAccountMenuOpened = getParseValue("isAccountMenuOpened", "boolean", $isAccountMenuOpened);
+    $isTodoOpened = getParseValue("isTodoOpened", "boolean", $isTodoOpened);
+    $isCompletedOpened = getParseValue("isCompletedOpened", "boolean", $isCompletedOpened);
+    $newTodo = getParseValue("newTodo", "string", $newTodo);
 
     // Let offline-svelte know that the client state has been restored in order
     // to restore scroll position.
@@ -38,5 +45,6 @@
     localStorage.setItem("isAccountMenuOpened", JSON.stringify($isAccountMenuOpened));
     localStorage.setItem("isTodoOpened", JSON.stringify($isTodoOpened));
     localStorage.setItem("isCompletedOpened", JSON.stringify($isCompletedOpened));
+    localStorage.setItem("newTodo", JSON.stringify($newTodo));
   }
 </script>
