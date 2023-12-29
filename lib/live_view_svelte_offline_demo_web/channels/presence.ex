910c1594550dd170eff6820ebca9a0a550ec73e5
Tony Dang <tony@tonydang.blog>
2023-12-29 11:27:24 -0800

    mv presence-related functions to Presence module

--- lib/live_view_svelte_offline_demo_web/channels/presence.ex
+++ lib/live_view_svelte_offline_demo_web/channels/presence.ex
@@ -8,4 +8,52 @@ defmodule LiveViewSvelteOfflineDemoWeb.Presence do
   use Phoenix.Presence,
     otp_app: :live_view_svelte_offline_demo,
     pubsub_server: LiveViewSvelteOfflineDemo.PubSub
+
+  # Public API _____________________________________________________________________________________
+
+  @doc """
+  Return count of sessions for the current user in the socket.
+  """
+  def get_session_count(socket) do
+    %{id: user_id} = socket.assigns.current_user
+    presences = list(presence_topic(user_id))
+    user_id_as_string = to_string(user_id)
+
+    case Map.has_key?(presences, user_id_as_string) do
+      true ->
+        %{^user_id_as_string => %{metas: metas}} = presences
+        length(metas)
+
+      _ ->
+        0
+    end
+  end
+
+  @doc """
+  Subscribe to presence updates for the given user id.
+  """
+  def subscribe(user_id) do
+    Phoenix.PubSub.subscribe(LiveViewSvelteOfflineDemo.PubSub, presence_topic(user_id))
+  end
+
+  @doc """
+  Track the presence of the current user in the socket.
+  """
+  def track_user_presence(socket) do
+    %{id: user_id} = socket.assigns.current_user
+    track(self(), presence_topic(user_id), user_id, %{})
+  end
+
+  @doc """
+  Untrack user presence.
+  """
+  def untrack_user_presence(socket) do
+    %{id: user_id} = socket.assigns.current_user
+    untrack(self(), presence_topic(user_id), user_id)
+  end
+
+  # Private Helpers ________________________________________________________________________________
+
+  # Returns the presence topic for a given user_id.
+  defp presence_topic(user_id), do: "presence:user_id:#{user_id}"
 end
