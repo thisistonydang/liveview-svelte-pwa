37f7456f5c895e5d9a1d18009cc1963346cd3699
Tony Dang <tony@tonydang.blog>
2023-12-29 12:41:42 -0800

    handle request_service_worker_version response from sw

--- assets/lib/offline-svelte/components/ServiceWorkerMessageHandler.svelte
+++ assets/lib/offline-svelte/components/ServiceWorkerMessageHandler.svelte
@@ -1,11 +1,16 @@
 <script>
   import { onMount } from "svelte";
+  import { serviceWorkerVersion } from "../lib/serviceWorkerVersion";
 
   onMount(() => {
     navigator.serviceWorker?.addEventListener("message", (event) => {
       switch (event.data.type) {
         case "request_skip_waiting":
           window.location.reload();
+          break;
+
+        case "request_service_worker_version":
+          $serviceWorkerVersion = event.data.payload.serviceWorkerVersion;
           break;
 
         default:
