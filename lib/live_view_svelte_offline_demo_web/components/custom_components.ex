f27dc67fe2f1fc77fb234fc7decac5ead96ec37b
Tony Dang <tony@tonydang.blog>
2023-12-31 14:57:50 -0800

    add app_info component

--- lib/live_view_svelte_offline_demo_web/components/custom_components.ex
+++ lib/live_view_svelte_offline_demo_web/components/custom_components.ex
@@ -54,4 +54,49 @@ defmodule LiveViewSvelteOfflineDemoWeb.CustomComponents do
     </html>
     """
   end
+
+  def app_info(assigns) do
+    ~H"""
+    <div class="hero min-h-screen">
+      <div class="hero-content">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-black my-5">Offline-Enabled LiveView Svelte Demo</h1>
+          <p>
+            This is a demo of a Phoenix LiveView
+            <abbr class="underline-offset-4" title="Progressive WebApp">PWA</abbr>
+            that can sync realtime across multiple devices while
+            also being able to fallback to working offline.
+          </p>
+
+          <div class="my-6">
+            <h2 class="text-3xl font-bold my-3">
+              It utilizes the following technologies
+            </h2>
+
+            <ul class="list-disc pl-4">
+              <li>Phoenix LiveView, Presence, and PubSub</li>
+              <li>Ecto/PostgreSQL</li>
+              <li>Svelte (via live_svelte)</li>
+              <li>Service Workers</li>
+              <li>LocalStorage</li>
+            </ul>
+          </div>
+
+          <h2 class="text-3xl font-bold my-3">
+            Register or log in to try it out
+          </h2>
+
+          <ul class="flex gap-2">
+            <li>
+              <a href={~p"/users/register"} class="btn btn-accent border border-neutral">Register</a>
+            </li>
+            <li>
+              <a href={~p"/users/log_in"} class="btn btn-accent border border-neutral">Log in</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    """
+  end
 end
