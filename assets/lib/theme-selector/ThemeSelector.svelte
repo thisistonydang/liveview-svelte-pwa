c94d62495ee485291b1ea58b82ae9f43745ab58c
Tony Dang <tony@tonydang.blog>
2023-12-31 13:14:57 -0800

    add ThemeSelector

---
+++ assets/lib/theme-selector/ThemeSelector.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  import { currentTheme } from "./lib/currentTheme";
+
+  import ThemeButton from "./components/ThemeButton.svelte";
+  import ThemeSyncManager from "./ThemeSyncManager.svelte";
+
+  onMount(() => {
+    const theme = JSON.parse(localStorage.getItem("theme"));
+
+    if (!theme) {
+      $currentTheme = "system";
+    } else {
+      $currentTheme = theme;
+    }
+  });
+</script>
+
+<ThemeButton />
+<ThemeSyncManager />
