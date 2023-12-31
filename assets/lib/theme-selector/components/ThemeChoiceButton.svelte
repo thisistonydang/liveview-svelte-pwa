48a2e99846f593d0a9e91267ca425d1c8a255803
Tony Dang <tony@tonydang.blog>
2023-12-31 13:02:59 -0800

    add ThemeChoiceButton

---
+++ assets/lib/theme-selector/components/ThemeChoiceButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { currentTheme } from "../lib/currentTheme";
+  import { setTheme } from "../lib/setTheme";
+
+  import CheckSvgIcon from "./CheckSvgIcon.svelte";
+
+  /** @type {"system" | "light" | "dark"} */
+  export let theme;
+</script>
+
+<li>
+  <button on:click={() => setTheme(theme)}>
+    <span class="first-letter:capitalize">{theme}</span>
+
+    {#if theme === $currentTheme}
+      <span class="w-3 h-3">
+        <CheckSvgIcon />
+      </span>
+    {/if}
+  </button>
+</li>
