b4b0faf6eca13d28c4d619f16328252d6395cce0
Tony Dang <tony@tonydang.blog>
2023-12-31 14:42:09 -0800

    add back link from account confirmation

--- lib/live_view_svelte_offline_demo_web/live/user_confirmation_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_confirmation_live.ex
@@ -5,20 +5,20 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserConfirmationLive do
 
   def render(%{live_action: :edit} = assigns) do
     ~H"""
-    <div class="mx-auto max-w-sm">
-      <.header class="text-center">Confirm Account</.header>
+    <div class="hero min-h-screen mx-auto max-w-md">
+      <div class="hero-content">
+        <div>
+          <.BackLink linkTo={~p"/"} linkText="Back" />
+          <.header class="text-center">Confirm Account</.header>
 
-      <.simple_form for={@form} id="confirmation_form" phx-submit="confirm_account">
-        <.input field={@form[:token]} type="hidden" />
-        <:actions>
-          <.button phx-disable-with="Confirming..." class="w-full">Confirm my account</.button>
-        </:actions>
-      </.simple_form>
-
-      <p class="text-center mt-4">
-        <.link href={~p"/users/register"}>Register</.link>
-        | <.link href={~p"/users/log_in"}>Log in</.link>
-      </p>
+          <.simple_form for={@form} id="confirmation_form" phx-submit="confirm_account">
+            <.input field={@form[:token]} type="hidden" />
+            <:actions>
+              <.button phx-disable-with="Confirming..." class="w-full">Confirm my account</.button>
+            </:actions>
+          </.simple_form>
+        </div>
+      </div>
     </div>
     """
   end
