c8c7631ece952efe62ae7f02d761007f197e1496
Tony Dang <tony@tonydang.blog>
2023-12-31 14:38:23 -0800

    add back link from password reset

--- lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
@@ -8,6 +8,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserResetPasswordLive do
     <div class="hero min-h-screen mx-auto max-w-sm">
       <div class="hero-content">
         <div>
+          <.BackLink linkTo={~p"/"} linkText="Back" />
           <.header class="text-center">Reset Password</.header>
 
           <.simple_form
@@ -31,11 +32,6 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserResetPasswordLive do
               <.button phx-disable-with="Resetting..." class="w-full">Reset Password</.button>
             </:actions>
           </.simple_form>
-
-          <p class="text-center text-sm mt-4">
-            <.link href={~p"/users/register"}>Register</.link>
-            | <.link href={~p"/users/log_in"}>Log in</.link>
-          </p>
         </div>
       </div>
     </div>
