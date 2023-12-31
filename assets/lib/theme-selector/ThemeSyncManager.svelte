472314387de94cf7f8b228704a41c1b36c6789ad
Tony Dang <tony@tonydang.blog>
2023-12-31 12:53:39 -0800

    add ThemeSyncManager

---
+++ assets/lib/theme-selector/ThemeSyncManager.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { setTheme } from "./lib/setTheme";
+</script>
+
+<!-- Keep theme synced when theme is updated in localStorage on another
+tab/window of the same browser. -->
+<svelte:window
+  on:storage={({ key, newValue }) => {
+    if (key !== "theme") return;
+
+    const theme = JSON.parse(newValue) || "system";
+    setTheme(theme);
+  }}
+/>
