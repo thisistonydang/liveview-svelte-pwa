999461660f5703aaa2f68e578b732810e2ed53be
Tony Dang <tony@tonydang.blog>
2023-12-31 14:11:14 -0800

    change back link to more accessible color in light mode

--- assets/svelte/BackLink.svelte
+++ assets/svelte/BackLink.svelte
@@ -10,7 +10,7 @@
 
 <div class="my-4">
   <a
-    class="link link-accent no-underline text-sm font-semibold flex items-center gap-1"
+    class="link no-underline text-sm font-semibold flex items-center gap-1"
     href={linkTo}
     on:click|preventDefault={() => {
       window.location.replace(linkTo);
