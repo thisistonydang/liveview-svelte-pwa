412ec2ac72292a60cfade6b226d21069febb2562
Tony Dang <tony@tonydang.blog>
2023-12-31 13:11:44 -0800

    add ThemeButton

---
+++ assets/lib/theme-selector/components/ThemeButton.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { clickOutside } from "lib/hooks/clickOutside";
+
+  import { isThemeMenuOpened } from "../lib/isThemeMenuOpened";
+  import SwatchSvgIcon from "./SwatchSvgIcon.svelte";
+  import ThemeChoiceButton from "./ThemeChoiceButton.svelte";
+</script>
+
+<details
+  class="dropdown dropdown-end"
+  bind:open={$isThemeMenuOpened}
+  use:clickOutside={() => ($isThemeMenuOpened = false)}
+>
+  <summary class="my-1 btn btn-circle btn-neutral" aria-label="Theme Selector">
+    <SwatchSvgIcon />
+  </summary>
+
+  <div class="dropdown-content menu bg-base-200 border border-neutral rounded-box">
+    <p class="px-4 py-2 font-bold border-b border-neutral rounded-none mb-1.5">Theme</p>
+
+    <ul class="w-28">
+      <ThemeChoiceButton theme="system" />
+      <ThemeChoiceButton theme="light" />
+      <ThemeChoiceButton theme="dark" />
+    </ul>
+  </div>
+</details>
