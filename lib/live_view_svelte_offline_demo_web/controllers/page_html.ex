765c1d1aa0e96c52056e6dc010f7bbf4c601bca6
Tony Dang <tony@tonydang.blog>
2024-01-01 15:14:47 -0800

    replace app_info w/ AppInfo on home page

--- lib/live_view_svelte_offline_demo_web/controllers/page_html.ex
+++ lib/live_view_svelte_offline_demo_web/controllers/page_html.ex
@@ -6,20 +6,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.PageHTML do
     <!-- This div is a spacer -->
     <div class="h-14" />
 
-    <.app_info>
-      <h2 class="text-3xl font-bold my-3">
-        Register or log in to try it out
-      </h2>
-
-      <ul class="flex gap-2">
-        <li>
-          <a href={~p"/users/register"} class="btn btn-accent border border-neutral">Register</a>
-        </li>
-        <li>
-          <a href={~p"/users/log_in"} class="btn btn-accent border border-neutral">Log in</a>
-        </li>
-      </ul>
-    </.app_info>
+    <.AppInfo showAuthLinks />
     """
   end
 end
