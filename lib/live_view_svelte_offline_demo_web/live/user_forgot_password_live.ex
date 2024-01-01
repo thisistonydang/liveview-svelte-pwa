e42c751090849e0ab1ec14bf5023d71d30b961a2
Tony Dang <tony@tonydang.blog>
2024-01-01 13:39:10 -0800

    update forgot password page to use user_auth_layout

--- lib/live_view_svelte_offline_demo_web/live/user_forgot_password_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_forgot_password_live.ex
@@ -9,7 +9,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserForgotPasswordLive do
       <.BackLink linkTo={~p"/"} />
     </.sticky_header>
 
-    <div class="max-w-sm mx-auto px-2 md:p-0 mt-10">
+    <.user_auth_layout>
       <.header class="text-center">
         Forgot your password?
         <:subtitle>We'll send a password reset link to your inbox</:subtitle>
@@ -23,7 +23,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserForgotPasswordLive do
           </.button>
         </:actions>
       </.simple_form>
-    </div>
+    </.user_auth_layout>
     """
   end
 
