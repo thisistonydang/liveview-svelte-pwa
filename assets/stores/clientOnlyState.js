43e957f78b20df5fe81f9083523893c980e3e29f
Tony Dang <tony@tonydang.blog>
2024-01-01 14:47:39 -0800

    add store to track whether About Page is opened

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
 
+export const isAboutPageOpened = writable(false);
 export const isAccountMenuOpened = writable(false);
 export const isTodoOpened = writable(true);
 export const isCompletedOpened = writable(false);
