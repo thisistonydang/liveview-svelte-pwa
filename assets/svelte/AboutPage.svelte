e3c4c8fe8f0bf49eec0a3c629cc93a75e8b03baa
Tony Dang <tony@tonydang.blog>
2024-01-01 15:10:06 -0800

    add AboutPage

---
+++ assets/svelte/AboutPage.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { isAboutPageOpened } from "../stores/clientOnlyState";
+
+  import AppInfo from "./AppInfo.svelte";
+  import ArrowLeftSvgIcon from "./ArrowLeftSvgIcon.svelte";
+</script>
+
+<div class="sticky top-0 left-0 w-full py-1 z-10 bg-base-100 border-neutral border-b h-14">
+  <div class="max-w-2xl mx-auto px-2 md:p-0 h-full flex items-center">
+    <button class="h-10 w-10" aria-label="Back" on:click={() => ($isAboutPageOpened = false)}>
+      <ArrowLeftSvgIcon />
+    </button>
+  </div>
+</div>
+
+<AppInfo />
