43fa72f48daabb6687e3c1653a1e2dcd5d0652d3
Tony Dang <tony@tonydang.blog>
2024-01-01 13:17:44 -0800

    update home page with new app_info

--- lib/live_view_svelte_offline_demo_web/controllers/page_html.ex
+++ lib/live_view_svelte_offline_demo_web/controllers/page_html.ex
@@ -3,21 +3,22 @@ defmodule LiveViewSvelteOfflineDemoWeb.PageHTML do
 
   def home(assigns) do
     ~H"""
-    <.app_info>
-      <:post_info>
-        <h2 class="text-3xl font-bold my-3">
-          Register or log in to try it out
-        </h2>
+    <!-- This div is a spacer -->
+    <div class="h-14" />
 
-        <ul class="flex gap-2">
-          <li>
-            <a href={~p"/users/register"} class="btn btn-accent border border-neutral">Register</a>
-          </li>
-          <li>
-            <a href={~p"/users/log_in"} class="btn btn-accent border border-neutral">Log in</a>
-          </li>
-        </ul>
-      </:post_info>
+    <.app_info>
+      <h2 class="text-3xl font-bold my-3">
+        Register or log in to try it out
+      </h2>
+
+      <ul class="flex gap-2">
+        <li>
+          <a href={~p"/users/register"} class="btn btn-accent border border-neutral">Register</a>
+        </li>
+        <li>
+          <a href={~p"/users/log_in"} class="btn btn-accent border border-neutral">Log in</a>
+        </li>
+      </ul>
     </.app_info>
     """
   end
