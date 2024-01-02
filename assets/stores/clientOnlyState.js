76464fb7308305b2e6e74cdf344f6b1b90e6f05b
Tony Dang <tony@tonydang.blog>
2024-01-02 15:30:33 -0800

    rm isAboutPageOpened store

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -1,6 +1,5 @@
 import { writable } from "svelte/store";
 
-export const isAboutPageOpened = writable(false);
 export const isAccountMenuOpened = writable(false);
 export const isTodoOpened = writable(true);
 export const isCompletedOpened = writable(false);
