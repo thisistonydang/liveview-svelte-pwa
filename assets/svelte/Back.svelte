aca620546853ce27c55b6d5c3d23b12051e54430
Tony Dang <tony@tonydang.blog>
2024-01-02 15:43:00 -0800

    add Back component

---
+++ assets/svelte/Back.svelte
@@ -0,0 +1,19 @@
+<script>
+  import ArrowLeftSvgIcon from "./ArrowLeftSvgIcon.svelte";
+
+  export let live;
+  live;
+
+  export let linkTo = undefined;
+  export let ariaLabel = "Back";
+</script>
+
+{#if linkTo}
+  <a href={linkTo} aria-label={ariaLabel}>
+    <ArrowLeftSvgIcon className="h-10 w-10" />
+  </a>
+{:else}
+  <button aria-label={ariaLabel} on:click={() => history.back()}>
+    <ArrowLeftSvgIcon className="h-10 w-10" />
+  </button>
+{/if}
