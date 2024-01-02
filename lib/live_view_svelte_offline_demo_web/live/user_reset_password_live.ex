0100097db94b144d573f03783f9ee280fa9eb90f
Tony Dang <tony@tonydang.blog>
2024-01-02 15:55:00 -0800

    replace BackLink -> Back

--- lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
@@ -6,7 +6,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserResetPasswordLive do
   def render(assigns) do
     ~H"""
     <.sticky_header>
-      <.BackLink linkTo={~p"/"} />
+      <.Back linkTo={~p"/"} />
     </.sticky_header>
 
     <.user_auth_layout>
