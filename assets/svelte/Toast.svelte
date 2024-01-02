992f086095e59fd7094ad63835c5ab7581bd1f14
Tony Dang <tony@tonydang.blog>
2024-01-01 16:58:33 -0800

    add Toast component

---
+++ assets/svelte/Toast.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { fly } from "svelte/transition";
+
+  import { toast } from "../stores/toast";
+
+  import InfoSvgIcon from "./InfoSvgIcon.svelte";
+  import WarningSvgIcon from "./WarningSvgIcon.svelte";
+  import XMarkSvgIcon from "./XMarkSvgIcon.svelte";
+</script>
+
+{#if $toast.show}
+  <label
+    transition:fly={{ x: 50 }}
+    role="alert"
+    class={`
+    fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 shadow-md
+    ${$toast.kind === "info" && "bg-success text-success-content fill-success"}
+    ${$toast.kind === "error" && "bg-error text-error-content fill-error"}
+  `}
+  >
+    <p class="flex items-center gap-1.5 text-sm font-semibold leading-6">
+      {#if $toast.kind === "info"}
+        <span class="h-4 w-4"><InfoSvgIcon /></span>
+      {:else if $toast.kind === "error"}
+        <span class="h-4 w-4"><WarningSvgIcon /></span>
+      {/if}
+
+      {$toast.title}
+    </p>
+
+    <p class="mt-2 text-sm leading-5">{$toast.msg}</p>
+
+    <button
+      class="group absolute top-1 right-1 p-2"
+      aria-label="close"
+      on:click={() => {
+        $toast = {
+          show: false,
+          kind: "",
+          title: "",
+          msg: "",
+        };
+      }}
+    >
+      <div class="h-5 w-5"><XMarkSvgIcon /></div>
+    </button>
+  </label>
+{/if}
