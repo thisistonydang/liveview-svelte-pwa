d092eebf21687fcc8a83b61815f28d526ca7f855
Tony Dang <tony@tonydang.blog>
2024-01-03 08:33:52 -0800

    rename component for clarity

--- assets/svelte/Logo.svelte
+++ assets/svelte/Logo.svelte
@@ -1,7 +1,7 @@
 <script>
-  import CheckSvgMicro from "lib/heroicons/CheckSvgMicro.svelte";
+  import CheckSvgIconMicro from "lib/heroicons/CheckSvgIconMicro.svelte";
 </script>
 
 <div class="h-screen w-screen bg-accent flex justify-center items-center">
-  <CheckSvgMicro fill="#2a323c" className="h-96 w-96" />
+  <CheckSvgIconMicro fill="#2a323c" className="h-96 w-96" />
 </div>
