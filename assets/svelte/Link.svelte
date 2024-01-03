f6f187368bdafaa08cc1c246d8ed5ec59325feb2
Tony Dang <tony@tonydang.blog>
2024-01-03 11:32:30 -0800

    add Link component

---
+++ assets/svelte/Link.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let href;
+  export let external = false;
+</script>
+
+<a
+  {href}
+  target={external ? "_blank" : undefined}
+  rel={external ? "noopener noreferrer" : undefined}
+  class="font-semibold underline underline-offset-4 hover:no-underline"
+>
+  <slot />
+</a>
