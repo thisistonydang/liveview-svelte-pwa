da67376167633dd337f77e48b6a4fa4efca40731
Tony Dang <tony@tonydang.blog>
2024-01-03 09:45:53 -0800

    update favicon assets

--- lib/live_view_svelte_offline_demo_web/components/custom_components.ex
+++ lib/live_view_svelte_offline_demo_web/components/custom_components.ex
@@ -21,7 +21,7 @@ defmodule LiveViewSvelteOfflineDemoWeb.CustomComponents do
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00ccb7" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="apple-mobile-web-app-title" content="ToDo" />
         <meta name="application-name" content="ToDo" />
         <meta name="msapplication-TileColor" content="#00aba9" />
