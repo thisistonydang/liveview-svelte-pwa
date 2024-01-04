ab8b54fda96cabecc70ef793679ff5e657f43171
Tony Dang <tony@tonydang.blog>
2024-01-04 14:20:05 -0800

    componentize TodoItemsContainer and add dnd functionality

--- assets/svelte/TodoApp.svelte
+++ assets/svelte/TodoApp.svelte
@@ -112,61 +112,28 @@
   <p class="text-error" in:fly={{ y: -10 }}>{error}</p>
 {/if}
 
-<div class="collapse collapse-arrow border border-neutral my-2">
-  <input id="todo" type="checkbox" bind:checked={$isTodoOpened} />
-  <label for="todo" class="collapse-title text-xl font-medium">
-    To Do ({$todoItems.length})
-  </label>
-  <div class="collapse-content">
-    {#if $todoItems.length}
-      <ul>
-        {#each $todoItems as item (item.id)}
-          <li
-            class="flex px-2 py-1.5 text-lg hover:bg-base-200 rounded-lg"
-            animate:flip
-            in:receive
-            out:send
-          >
-            <label class="flex items-center gap-3 cursor-pointer">
-              <input class="checkbox" type="checkbox" on:change={() => checkItem(item)} />
-              <span>{item.name}</span>
-            </label>
-          </li>
-        {/each}
-      </ul>
-    {:else}
-      <p in:fade={{ delay: 250 }}>All done!</p>
-    {/if}
-  </div>
-</div>
+<TodoItemsContainer
+  title="To Do"
+  bind:isDropdownOpened={$isTodoOpened}
+  items={$todoItems}
+  itemsStore={todoItems}
+  checkHandler={checkItem}
+  {deleteItem}
+  {handleConsider}
+  {handleFinalize}
+  bind:dragDisabled
+  noItemsMessage="All done!"
+/>
 
-<div class="collapse collapse-arrow border border-neutral my-2">
-  <input id="completed" type="checkbox" bind:checked={$isCompletedOpened} />
-  <label for="completed" class="collapse-title text-xl font-medium">
-    Completed ({$completedItems.length})
-  </label>
-  <div class="collapse-content">
-    {#if $completedItems.length}
-      <ul>
-        {#each $completedItems as item (item.id)}
-          <li
-            class="flex items-center justify-between px-2 py-1.5 text-lg hover:bg-base-200 rounded-lg"
-            animate:flip
-            in:receive
-            out:send
-          >
-            <label class="flex items-center gap-3 cursor-pointer">
-              <input class="checkbox" type="checkbox" checked on:change={() => uncheckItem(item)} />
-              <span class="line-through">{item.name}</span>
-            </label>
-            <button aria-label="Delete item." on:click={() => deleteItem(item)}>
-              <XMarkSvgIcon className="w-6 h-6" />
-            </button>
-          </li>
-        {/each}
-      </ul>
-    {:else}
-      <p in:fade={{ delay: 250 }}>No completed items.</p>
-    {/if}
-  </div>
-</div>
+<TodoItemsContainer
+  title="Completed"
+  bind:isDropdownOpened={$isCompletedOpened}
+  items={$completedItems}
+  itemsStore={completedItems}
+  checkHandler={uncheckItem}
+  {deleteItem}
+  {handleConsider}
+  {handleFinalize}
+  bind:dragDisabled
+  noItemsMessage="No completed items."
+/>
---
+++ assets/svelte/TodoItemsContainer.svelte
@@ -0,0 +1,114 @@
+<script>
+  import { flip } from "svelte/animate";
+  import { fade } from "svelte/transition";
+  import { dndzone } from "svelte-dnd-action";
+
+  import { clickOutside } from "lib/hooks/clickOutside";
+  import Bars3SvgIcon from "lib/svg-icons/Bars3SvgIcon.svelte";
+  import XMarkSvgIcon from "lib/svg-icons/XMarkSvgIcon.svelte";
+  import ChevronDownSvgIcon from "lib/svg-icons/ChevronDownSvgIcon.svelte";
+  import ChevronUpSvgIcon from "lib/svg-icons/ChevronUpSvgIcon.svelte";
+
+  export let title;
+  export let isDropdownOpened;
+  export let items;
+  export let itemsStore;
+  export let checkHandler;
+  export let deleteItem;
+  export let handleConsider;
+  export let handleFinalize;
+  export let dragDisabled;
+  export let noItemsMessage;
+
+  const flipDurationMs = 100;
+
+  function startDrag(event) {
+    // Preventing default to prevent lag on touch devices (because of the
+    // browser checking for screen scrolling).
+    event.preventDefault();
+    dragDisabled = false;
+  }
+
+  function handleKeyDown(e) {
+    if ((e.key === "Enter" || e.key === " ") && dragDisabled) {
+      dragDisabled = false;
+    }
+  }
+</script>
+
+<div class="collapse border border-neutral my-2">
+  <!-- This hidden checkbox controls the collapse via Daisy UI. -->
+  <input type="checkbox" class="pointer-events-none" bind:checked={isDropdownOpened} />
+
+  <!-- Collapse title. -->
+  <div class="collapse-title relative" style="cursor: default;">
+    <div class="flex gap-2 items-center text-xl font-medium">
+      <span>{title}</span>
+      <span class="badge badge-neutral">{items.length}</span>
+    </div>
+
+    <!-- Collapse toggle. -->
+    <label class="cursor-pointer swap swap-rotate absolute top-[18px] right-5">
+      <!-- This checkbox toggles the above Daisy UI checkbox. -->
+      <input type="checkbox" class="hidden" bind:checked={isDropdownOpened} />
+      <ChevronDownSvgIcon className="swap-off w-6 h-6" />
+      <ChevronUpSvgIcon className="swap-on w-6 h-6" />
+    </label>
+  </div>
+
+  <div class="collapse-content">
+    <ul
+      class="min-h-[40px]"
+      aria-label={title}
+      use:dndzone={{
+        items,
+        flipDurationMs,
+        dragDisabled: !items.length || dragDisabled,
+        dropTargetStyle: {},
+        dropTargetClasses: ["border-2", "border-dashed", "rounded-lg", "border-accent"],
+      }}
+      on:consider={(event) => handleConsider(event, itemsStore)}
+      on:finalize={(event) => handleFinalize(event, itemsStore)}
+    >
+      {#each items as item (item.id)}
+        <li
+          class="flex items-center justify-between px-2 py-1.5 text-lg hover:bg-base-200 rounded-lg"
+          aria-label={item.name}
+          animate:flip={{ duration: flipDurationMs }}
+        >
+          <span class="flex items-center gap-3">
+            <input
+              class="checkbox"
+              type="checkbox"
+              checked={title === "Completed"}
+              on:change={() => checkHandler(item)}
+            />
+
+            <span class:line-through={title === "Completed"}>{item.name}</span>
+          </span>
+
+          <div class="flex gap-1">
+            <button aria-label="Delete item." on:click={() => deleteItem(item)}>
+              <XMarkSvgIcon className="w-6 h-6" />
+            </button>
+
+            <!-- Drag Handle. -->
+            <button
+              aria-label="drag-handle"
+              class:cursor-grab={dragDisabled}
+              class:cursor-grabbing={!dragDisabled}
+              on:mousedown={startDrag}
+              on:touchstart={startDrag}
+              on:keydown={handleKeyDown}
+              use:clickOutside={() => (dragDisabled = true)}
+            >
+              <Bars3SvgIcon className="w-6 h-6" />
+            </button>
+          </div>
+        </li>
+      {:else}
+        <p in:fade={{ delay: 250 }}>{noItemsMessage}</p>
+      {/each}
+    </ul>
+  </div>
+</div>
