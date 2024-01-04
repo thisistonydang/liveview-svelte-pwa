930f33b1eba4c43940ff0c280b66ab6a8a141d48
Tony Dang <tony@tonydang.blog>
2024-01-04 12:53:56 -0800

    add dnd handlers

--- assets/svelte/TodoApp.svelte
+++ assets/svelte/TodoApp.svelte
@@ -47,6 +47,54 @@
     $todoItems = [item, ...$todoItems];
     syncClientToServer($todoItems, $completedItems, $liveView);
   }
+
+  function handleConsider(event, itemsStore) {
+    const newItems = filterDuplicates(event.detail.items);
+    const { source, trigger } = event.detail.info;
+
+    itemsStore.set(newItems);
+
+    // Ensure dragging is stopped on drag finish via keyboard
+    if (source === SOURCES.KEYBOARD && trigger === TRIGGERS.DRAG_STOPPED) {
+      dragDisabled = true;
+    }
+  }
+
+  function handleFinalize(event, itemsStore) {
+    const newItems = filterDuplicates(event.detail.items);
+    const { source } = event.detail.info;
+
+    // TODO: Is it necessary to check that the id is reset back from the
+    // svelte-dnd-action placeholder id? If the id is not reset, then
+    // it is possible to have duplicate ids which will crash the app.
+
+    itemsStore.set(newItems);
+
+    // Ensure dragging is stopped on drag finish via pointer (mouse, touch)
+    if (source === SOURCES.POINTER) {
+      dragDisabled = true;
+    }
+
+    syncClientToServer($todoItems, $completedItems, $liveView);
+  }
+
+  /**
+   * Helper function to remove any items with duplicate ids from the items array.
+   *
+   * @param {TodoItem[]} items
+   */
+  function filterDuplicates(items) {
+    /** @type {string[]} */
+    const ids = [];
+
+    return items.filter((item) => {
+      if (!ids.includes(item.id)) {
+        ids.push(item.id);
+        return true;
+      }
+      return false;
+    });
+  }
 </script>
 
 <form on:submit|preventDefault={addItem} class="join my-2 w-full">
