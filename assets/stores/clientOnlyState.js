0053ffbc6120059b0f1ac1f8cf478bf6d28a1b28
Tony Dang <tony@tonydang.blog>
2024-01-06 15:32:09 -0800

    add openedOptionsMenuId store

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -4,3 +4,4 @@ export const isAccountMenuOpened = writable(false);
 export const isTodoOpened = writable(true);
 export const isCompletedOpened = writable(false);
 export const newTodo = writable("");
+export const openedOptionsMenuId = writable("");
