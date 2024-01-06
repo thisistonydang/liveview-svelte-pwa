53b41acd0ffb5784c80166ec4c5122df38bec769
Tony Dang <tony@tonydang.blog>
2024-01-06 15:33:40 -0800

    refactor

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 
 export const isAccountMenuOpened = writable(false);
-export const isTodoOpened = writable(true);
 export const isCompletedOpened = writable(false);
+export const isTodoOpened = writable(true);
 export const newTodo = writable("");
 export const openedOptionsMenuId = writable("");
