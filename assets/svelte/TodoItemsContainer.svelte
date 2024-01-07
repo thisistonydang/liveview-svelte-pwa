a39f8531083f053fe97b40567b823141d1987def
Tony Dang <tony@tonydang.blog>
2024-01-06 16:30:15 -0800

    styling

--- assets/svelte/TodoItemsContainer.svelte
+++ assets/svelte/TodoItemsContainer.svelte
@@ -125,7 +125,7 @@
           {/if}
         </li>
       {:else}
-        <p class="flex items-center h-10" in:fade={{ delay: 250 }}>{noItemsMessage}</p>
+        <p class="flex items-center h-10 px-2" in:fade={{ delay: 250 }}>{noItemsMessage}</p>
       {/each}
     </ul>
   </div>
