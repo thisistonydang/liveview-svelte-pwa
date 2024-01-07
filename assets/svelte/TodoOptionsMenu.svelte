cc81d87bd0c207fad0046f3082b2aa706d9a7ec2
Tony Dang <tony@tonydang.blog>
2024-01-06 16:11:56 -0800

    add TodoOptionsMenu

---
+++ assets/svelte/TodoOptionsMenu.svelte
@@ -0,0 +1,53 @@
+<script>
+  import EllipsisHorizontalCircleSvgIcon from "lib/svg-icons/EllipsisHorizontalCircleSvgIcon.svelte";
+  import PencilSvgIcon from "lib/svg-icons/PencilSvgIcon.svelte";
+  import TrashSvgIcon from "lib/svg-icons/TrashSvgIcon.svelte";
+
+  import { openedOptionsMenuId } from "../stores/clientOnlyState";
+
+  export let item;
+  export let itemsStore;
+  export let updateItem;
+  export let deleteItem;
+  export let todoOptionsMenuClass;
+</script>
+
+<div class="{todoOptionsMenuClass} relative">
+  <button
+    class="flex items-center"
+    aria-label="Toggle options menu."
+    on:click={() => ($openedOptionsMenuId = $openedOptionsMenuId === item.id ? "" : item.id)}
+  >
+    <EllipsisHorizontalCircleSvgIcon className="w-6 h-6" />
+  </button>
+
+  {#if $openedOptionsMenuId === item.id}
+    <div class="absolute right-8 -top-3 menu bg-base-200 border border-neutral rounded-box">
+      <div class="flex items-center gap-2">
+        <button
+          class="flex items-center gap-1 p-2 rounded-lg hover:bg-neutral"
+          on:click={() => deleteItem(itemsStore, item)}
+        >
+          <TrashSvgIcon className="w-4 h-4" />
+          Delete
+        </button>
+
+        <div class="bg-neutral-content opacity-75 h-6 w-[1px]" />
+
+        <button
+          class="flex items-center gap-1 p-2 rounded-lg hover:bg-neutral"
+          on:click={() =>
+            updateItem(itemsStore, {
+              id: item.id,
+              name: item.name,
+              newName: item.name,
+              isEditing: true,
+            })}
+        >
+          <PencilSvgIcon className="w-4 h-4" />
+          Edit
+        </button>
+      </div>
+    </div>
+  {/if}
+</div>
