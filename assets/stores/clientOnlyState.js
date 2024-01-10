3af2b538a0153c9bb13b253cae5ee2d212fe7d6d
Tony Dang <tony@tonydang.blog>
2024-01-10 10:08:23 -0800

    add activeTab store

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
 
+export const activeTab = writable("To-Do"); // "To-Do" | "Lists"
 export const isAccountMenuOpened = writable(false);
 export const isTodoOpened = writable(true);
 export const newTodo = writable("");
