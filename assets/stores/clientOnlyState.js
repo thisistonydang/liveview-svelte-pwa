5a854d6e12a7a61928eb4a15ee09120541c048dd
Tony Dang <tony@tonydang.blog>
2024-01-10 10:15:17 -0800

    add newList store

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -3,5 +3,6 @@ import { writable } from "svelte/store";
 export const activeTab = writable("To-Do"); // "To-Do" | "Lists"
 export const isAccountMenuOpened = writable(false);
 export const isTodoOpened = writable(true);
+export const newList = writable("");
 export const newTodo = writable("");
 export const openedOptionsMenuId = writable("");
