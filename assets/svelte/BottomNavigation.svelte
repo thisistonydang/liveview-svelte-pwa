8575ae25ac41c532ab6aff062a22239e28b1fddb
Tony Dang <tony@tonydang.blog>
2024-01-11 09:05:53 -0800

    add bg color to bottom nav

--- assets/svelte/BottomNavigation.svelte
+++ assets/svelte/BottomNavigation.svelte
@@ -5,13 +5,21 @@
   import { activeTab } from "../stores/clientOnlyState";
 </script>
 
-<div class="btm-nav max-w-2xl mx-auto px-2 md:p-0">
-  <button class:active={$activeTab === "To-Do"} on:click={() => ($activeTab = "To-Do")}>
+<div class="btm-nav max-w-2xl mx-auto">
+  <button
+    class="bg-base-200"
+    class:active={$activeTab === "To-Do"}
+    on:click={() => ($activeTab = "To-Do")}
+  >
     <CheckSvgIconMicro className="w-5 h-5" />
     <span class="btm-nav-label">To-Do</span>
   </button>
 
-  <button class:active={$activeTab === "Lists"} on:click={() => ($activeTab = "Lists")}>
+  <button
+    class="bg-base-200"
+    class:active={$activeTab === "Lists"}
+    on:click={() => ($activeTab = "Lists")}
+  >
     <ListBulletSvgIcon className="w-5 h-5" />
     <span class="btm-nav-label">Lists</span>
   </button>
