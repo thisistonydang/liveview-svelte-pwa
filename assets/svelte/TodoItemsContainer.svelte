f0b2ff89d028f44f186a174b0d7001521dcaedc5
Tony Dang <tony@tonydang.blog>
2024-01-11 09:26:32 -0800

    extract TodoCheckList component

--- assets/svelte/TodoItemsContainer.svelte
+++ assets/svelte/TodoItemsContainer.svelte
@@ -1,15 +1,8 @@
 <script>
-  import { flip } from "svelte/animate";
-  import { fade } from "svelte/transition";
-  import { dndzone } from "svelte-dnd-action";
-
-  import { clickOutside } from "lib/actions/clickOutside";
-  import Bars3SvgIcon from "lib/svg-icons/Bars3SvgIcon.svelte";
   import ChevronDownSvgIcon from "lib/svg-icons/ChevronDownSvgIcon.svelte";
   import ChevronUpSvgIcon from "lib/svg-icons/ChevronUpSvgIcon.svelte";
 
-  import EditForm from "./EditForm.svelte";
-  import OptionsMenu from "./OptionsMenu.svelte";
+  import TodoCheckList from "./TodoCheckList.svelte";
 
   export let title;
   export let isDropdownOpened;
@@ -48,64 +41,20 @@
   </div>
 
   <div class="collapse-content">
-    <ul
-      class="min-h-[40px]"
-      aria-label={title}
-      use:dndzone={{
-        items: $itemsStore,
-        flipDurationMs,
-        dragDisabled,
-        morphDisabled: true,
-        dropTargetStyle: {},
-        dropTargetClasses: ["border-2", "border-dashed", "rounded-lg", "border-accent"],
-      }}
-      on:consider={(event) => handleConsider(event, itemsStore)}
-      on:finalize={(event) => handleFinalize(event, itemsStore)}
-    >
-      {#each $itemsStore as item (item.id)}
-        <li
-          class="flex items-center justify-between px-2 py-1.5 text-lg rounded-lg"
-          aria-label={item.name}
-          animate:flip={{ duration: flipDurationMs }}
-        >
-          {#if item.isEditing}
-            <EditForm {item} {itemsStore} {updateItem} />
-          {:else}
-            <label
-              class="flex items-center gap-3 grow cursor-pointer"
-              class:opacity-50={item.completed}
-            >
-              <input
-                class="checkbox bg-transparent"
-                type="checkbox"
-                checked={item.completed}
-                on:change={() => toggleCompleted(item)}
-              />
-
-              <span class:line-through={item.completed}>{item.name}</span>
-            </label>
-
-            <div class="flex gap-1">
-              <OptionsMenu {item} {itemsStore} {updateItem} {deleteItem} {optionsMenuClass} />
-
-              <!-- Drag Handle. -->
-              <button
-                aria-label="drag-handle"
-                class:cursor-grab={dragDisabled}
-                class:cursor-grabbing={!dragDisabled}
-                on:mousedown={handleStartDrag}
-                on:touchstart={handleStartDrag}
-                on:keydown={handleDragKeyDown}
-                use:clickOutside={() => (dragDisabled = true)}
-              >
-                <Bars3SvgIcon className="w-6 h-6" />
-              </button>
-            </div>
-          {/if}
-        </li>
-      {:else}
-        <p class="flex items-center h-10 px-2" in:fade={{ delay: 250 }}>{noItemsMessage}</p>
-      {/each}
-    </ul>
+    <TodoCheckList
+      {title}
+      {itemsStore}
+      {toggleCompleted}
+      {updateItem}
+      {deleteItem}
+      {handleConsider}
+      {handleFinalize}
+      {dragDisabled}
+      {handleStartDrag}
+      {handleDragKeyDown}
+      {flipDurationMs}
+      {noItemsMessage}
+      {optionsMenuClass}
+    />
   </div>
 </div>
