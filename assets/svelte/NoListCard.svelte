4f18f364414e40a1161010282610569529a7f9bd
Tony Dang <tony@tonydang.blog>
2024-01-11 12:44:14 -0800

    add NoListCard

---
+++ assets/svelte/NoListCard.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { activeTab } from "../stores/clientOnlyState";
+</script>
+
+<div class="mx-auto my-20 xs:w-96 card bg-neutral text-neutral-content">
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">No lists yet.</h2>
+
+    <p class="mb-1">Please create a list to get started.</p>
+
+    <div class="card-actions">
+      <button class="btn btn-accent" on:click={() => ($activeTab = "Lists")}>Create</button>
+    </div>
+  </div>
+</div>
