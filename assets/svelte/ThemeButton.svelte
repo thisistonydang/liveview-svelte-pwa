8c335b63aa26e64f56e59ecb24f89bb839f9c503
Tony Dang <tony@tonydang.blog>
2024-01-12 13:05:47 -0800

    use openMenuId to control theme menu open status

--- assets/svelte/ThemeButton.svelte
+++ assets/svelte/ThemeButton.svelte
@@ -10,22 +10,24 @@
   const themeMenuId = "theme-menu-id";
 </script>
 
-<details
-  class="dropdown dropdown-end"
-  bind:open={$isThemeMenuOpened}
-  use:clickOutside={() => ($isThemeMenuOpened = false)}
->
-  <summary class="my-1 btn btn-circle btn-neutral" aria-label="Theme Selector">
+<div class="{menuClass} relative">
+  <button
+    class="my-1 btn btn-circle btn-neutral"
+    aria-label="Theme Selector"
+    on:click={() => ($openedMenuId = $openedMenuId === themeMenuId ? "" : themeMenuId)}
+  >
     <SwatchSvgIcon className="h-6 w-6" />
-  </summary>
+  </button>
 
-  <div class="dropdown-content menu bg-base-200 border border-neutral rounded-box">
-    <p class="px-4 py-2 font-bold border-b border-neutral rounded-none mb-1.5">Theme</p>
+  {#if $openedMenuId === themeMenuId}
+    <div class="menu bg-base-200 border border-neutral rounded-box absolute right-0">
+      <p class="px-4 py-2 font-bold border-b border-neutral rounded-none mb-1.5">Theme</p>
 
-    <ul class="w-28">
-      <ThemeChoiceButton theme="system" />
-      <ThemeChoiceButton theme="light" />
-      <ThemeChoiceButton theme="dark" />
-    </ul>
-  </div>
-</details>
+      <ul class="w-28">
+        <ThemeChoiceButton theme="system" />
+        <ThemeChoiceButton theme="light" />
+        <ThemeChoiceButton theme="dark" />
+      </ul>
+    </div>
+  {/if}
+</div>
