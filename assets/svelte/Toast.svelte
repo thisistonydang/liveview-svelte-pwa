127618afb39f3d60e103089cc64f40b6b21d0580
Tony Dang <tony@tonydang.blog>
2024-01-14 15:38:18 -0800

    Add click outside functionality to Toast component

--- assets/svelte/Toast.svelte
+++ assets/svelte/Toast.svelte
@@ -1,22 +1,33 @@
 <script>
   import { fly } from "svelte/transition";
 
+  import { clickOutside } from "lib/actions/clickOutside";
   import InfoSvgIcon from "lib/svg-icons/InfoSvgIcon.svelte";
   import WarningSvgIcon from "lib/svg-icons/WarningSvgIcon.svelte";
   import XMarkSvgIcon from "lib/svg-icons/XMarkSvgIcon.svelte";
 
   import { toast } from "../stores/toast";
+
+  function dismissToast() {
+    $toast = {
+      show: false,
+      kind: "",
+      title: "",
+      msg: "",
+    };
+  }
 </script>
 
 {#if $toast.show}
-  <label
+  <div
     transition:fly={{ x: 50 }}
     role="alert"
     class={`
-    fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 shadow-md
-    ${$toast.kind === "info" && "bg-success text-success-content fill-success"}
-    ${$toast.kind === "error" && "bg-error text-error-content fill-error"}
-  `}
+      fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 shadow-md
+      ${$toast.kind === "info" && "bg-success text-success-content fill-success"}
+      ${$toast.kind === "error" && "bg-error text-error-content fill-error"}
+    `}
+    use:clickOutside={dismissToast}
   >
     <p class="flex items-center gap-1.5 text-sm font-semibold leading-6">
       {#if $toast.kind === "info"}
@@ -30,19 +41,8 @@
 
     <p class="mt-2 text-sm leading-5">{$toast.msg}</p>
 
-    <button
-      class="group absolute top-1 right-1 p-2"
-      aria-label="close"
-      on:click={() => {
-        $toast = {
-          show: false,
-          kind: "",
-          title: "",
-          msg: "",
-        };
-      }}
-    >
+    <button class="group absolute top-1 right-1 p-2" aria-label="close" on:click={dismissToast}>
       <XMarkSvgIcon className="w-5 h-5" />
     </button>
-  </label>
+  </div>
 {/if}
