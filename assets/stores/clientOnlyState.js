726488deee9f8b7097a0fb6d3ad51e4a032e1e97
Tony Dang <tony@tonydang.blog>
2024-01-14 08:56:02 -0800

    add moveTodoId store

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -3,6 +3,7 @@ import { writable } from "svelte/store";
 export const activeTab = writable("To-Do"); // "To-Do" | "Lists"
 export const isListsOpened = writable(true);
 export const isTodoOpened = writable(true);
+export const moveTodoId = writable("");
 export const newList = writable("");
 export const newTodo = writable("");
 export const openedMenuId = writable("");
