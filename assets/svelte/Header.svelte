f118330574d314ed751be543a9d84dcb2a2fc93b
Tony Dang <tony@tonydang.blog>
2024-01-16 12:15:57 -0800

    Update header and sticky header height

--- assets/svelte/Header.svelte
+++ assets/svelte/Header.svelte
@@ -17,7 +17,7 @@
 <svelte:window bind:scrollY />
 
 <div
-  class="sticky top-0 left-0 w-full py-1 z-10 bg-base-100 border-neutral"
+  class="sticky top-0 left-0 w-full py-2 z-10 bg-base-100 border-neutral"
   class:border-b={scrollY > 0}
 >
   <div class="max-w-2xl mx-auto px-2 md:p-0 flex justify-between items-center">
--- assets/svelte/StickyHeader.svelte
+++ assets/svelte/StickyHeader.svelte
@@ -8,7 +8,7 @@
 <svelte:window bind:scrollY />
 
 <div
-  class="sticky top-0 left-0 w-full py-1 z-10 bg-base-100 border-neutral h-16"
+  class="sticky top-0 left-0 w-full z-10 bg-base-100 border-neutral h-[72px]"
   class:border-b={scrollY > 0}
 >
   <div class="max-w-2xl mx-auto px-2 md:p-0 h-full flex items-center">
