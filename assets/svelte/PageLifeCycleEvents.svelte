c6eeec9949a3460e4c526649d6bef23e2e525138
Tony Dang <tony@tonydang.blog>
2024-01-16 14:47:00 -0800

    Remove unused PageLifeCycleEvents component

--- assets/svelte/PageLifeCycleEvents.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  export let live;
-</script>
-
-<svelte:window
-  on:visibilitychange={() => {
-    live.pushEvent("visibility_change", { visibilityState: document.visibilityState });
-  }}
-/>
