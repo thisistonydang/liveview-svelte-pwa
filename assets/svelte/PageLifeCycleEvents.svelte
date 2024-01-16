c646b3339ea042f524cac0cbe831b82014f29292
Tony Dang <tony@tonydang.blog>
2024-01-16 14:21:22 -0800

    don't push before_unload events to server

--- assets/svelte/PageLifeCycleEvents.svelte
+++ assets/svelte/PageLifeCycleEvents.svelte
@@ -3,9 +3,6 @@
 </script>
 
 <svelte:window
-  on:beforeunload={() => {
-    live.pushEvent("before_unload");
-  }}
   on:visibilitychange={() => {
     live.pushEvent("visibility_change", { visibilityState: document.visibilityState });
   }}
