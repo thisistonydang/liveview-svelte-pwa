42ec87f3db203b56bba4905e2cf41a488c81389e
Tony Dang <tony@tonydang.blog>
2024-01-17 09:08:43 -0800

    Add types for svelte-dnd-action

---
+++ assets/global.d.ts
@@ -0,0 +1,10 @@
+// Types for svelte-dnd-action
+// https://github.com/isaacHagoel/svelte-dnd-action?tab=readme-ov-file#typescript
+declare type Item = import("svelte-dnd-action").Item;
+declare type DndEvent<ItemType = Item> = import("svelte-dnd-action").DndEvent<ItemType>;
+declare namespace svelteHTML {
+  interface HTMLAttributes<T> {
+    "on:consider"?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
+    "on:finalize"?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
+  }
+}
