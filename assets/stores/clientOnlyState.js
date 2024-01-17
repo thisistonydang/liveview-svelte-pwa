f1a39b62e3909f58dde71a8b224db7c8ae51ec6a
Tony Dang <tony@tonydang.blog>
2024-01-17 08:31:41 -0800

    add typing

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -7,4 +7,6 @@ export const newList = writable("");
 export const newTodo = writable("");
 export const openedMenuId = writable("");
 export const selectedListId = writable("");
-export const urlHash = writable(""); // "" | "about" | "listId"
+
+/** @type {import("svelte/store").Writable<"" | "about" | "listId">} */
+export const urlHash = writable("");
