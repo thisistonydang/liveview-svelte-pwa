ab5eb7b0ad84fd2d6be2461f3905c3ddcf4e11ec
Tony Dang <tony@tonydang.blog>
2024-01-18 13:31:39 -0800

    rename moveTodoId -> itemToProcessId

--- assets/stores/clientOnlyState.js
+++ assets/stores/clientOnlyState.js
@@ -2,7 +2,7 @@ import { writable } from "svelte/store";
 
 export const isListsOpened = writable(true);
 export const isTodoOpened = writable(true);
-export const moveTodoId = writable("");
+export const itemToProcessId = writable("");
 export const newList = writable("");
 export const newTodo = writable("");
 export const openedMenuId = writable("");
