ab2d628cce1cb061e4da9c0206152f3fe309c83a
Tony Dang <tony@tonydang.blog>
2024-01-18 15:45:59 -0800

    fix chrome a11y warnings

--- lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
+++ lib/live_view_svelte_offline_demo_web/live/user_reset_password_live.ex
@@ -22,13 +22,22 @@ defmodule LiveViewSvelteOfflineDemoWeb.UserResetPasswordLive do
           Oops, something went wrong! Please check the errors below.
         </.error>
 
-        <.input field={@form[:password]} type="password" label="New password" required />
+        <.input
+          field={@form[:password]}
+          type="password"
+          label="New password"
+          required
+          autocomplete="off"
+        />
+
         <.input
           field={@form[:password_confirmation]}
           type="password"
           label="Confirm new password"
           required
+          autocomplete="off"
         />
+
         <:actions>
           <.button phx-disable-with="Resetting..." class="w-full">Reset Password</.button>
         </:actions>
