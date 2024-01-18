0ab42f0daae1bd3a16e38364f0c38f54791a2d1c
Tony Dang <tony@tonydang.blog>
2024-01-18 09:34:45 -0800

    format

--- lib/live_view_svelte_offline_demo/user_states/user_state.ex
+++ lib/live_view_svelte_offline_demo/user_states/user_state.ex
@@ -93,6 +93,7 @@ defmodule LiveViewSvelteOfflineDemo.UserStates.UserState do
   defp is_valid_id_and_name?(item) do
     is_valid_uuid?(item["id"]) && is_valid_name?(item["name"])
   end
+
   def is_valid_new_name?(item) do
     Map.has_key?(item, "newName") && is_valid_name?(item["newName"])
   end
