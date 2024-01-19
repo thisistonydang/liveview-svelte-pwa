6480699b541a5eb176ea7232c00211ce1817334f
Tony Dang <tony@tonydang.blog>
2024-01-19 15:24:39 -0800

    rm unused prop

--- assets/lib/offline-svelte/OfflineSvelte.svelte
+++ assets/lib/offline-svelte/OfflineSvelte.svelte
@@ -1,8 +1,6 @@
 <script>
   import ServiceWorkerMessageHandler from "./components/ServiceWorkerMessageHandler.svelte";
   import ServiceWorkerUpdater from "./components/ServiceWorkerUpdater.svelte";
-
-  export let scrollPositionKey = "scrollPosition";
 </script>
 
 <ServiceWorkerMessageHandler />
