20c53625ce6bb7fcd877a60d012179b7e14c4b83
Tony Dang <tony@tonydang.blog>
2024-01-21 12:26:23 -0800

    sw to ts

--- assets/lib/offline-svelte/OfflineSvelte.svelte
+++ assets/lib/offline-svelte/OfflineSvelte.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import ScrollPositionRestorer from "./components/ScrollPositionRestorer.svelte";
   import ServiceWorkerMessageHandler from "./components/ServiceWorkerMessageHandler.svelte";
   import ServiceWorkerUpdater from "./components/ServiceWorkerUpdater.svelte";
