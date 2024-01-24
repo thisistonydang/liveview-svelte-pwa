a1d3cdf0d90cb0c6ae79883f3831025f73a48a13
Tony Dang <tony@tonydang.blog>
2024-01-24 12:25:26 -0800

    add isHTMLElement type guard.

--- assets/lib/actions/focusTrap.ts
+++ assets/lib/actions/focusTrap.ts
@@ -2,3 +2,11 @@ interface Options {
   focusFirstElement?: boolean;
   onEscape?: () => void;
 }
+
+/**
+ * Type guard to check if an element is an HTMLElement.
+ */
+function isHTMLElement(element: Element | HTMLElement): element is HTMLElement {
+  return element instanceof HTMLElement;
+}
+
