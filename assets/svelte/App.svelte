3735f5f712e265f22d7cd6413cab9c4c5d0f462a
Tony Dang <tony@tonydang.blog>
2024-01-24 09:05:51 -0800

    redirect '/' to '/app' in sw rather than using replaceState

--- assets/svelte/App.svelte
+++ assets/svelte/App.svelte
@@ -55,12 +55,6 @@
     });
 
     requestServiceWorkerVersion();
-
-    // Change URL to "/app" if it's not already. This is for the case where the service worker
-    // serves the cached "/app" route when the client requests the root "/" route.
-    if (window.location.pathname !== "/app") {
-      history.replaceState(null, "", "/app");
-    }
   });
 </script>
 
