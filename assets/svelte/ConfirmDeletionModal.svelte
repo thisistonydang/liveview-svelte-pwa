204dddb4bacb9b40619e70eb7108915be2432c8c
Tony Dang <tony@tonydang.blog>
2024-01-24 14:05:28 -0800

    add focusTrap to ConfirmDeletionModal

--- assets/svelte/ConfirmDeletionModal.svelte
+++ assets/svelte/ConfirmDeletionModal.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  import { focusTrap } from "lib/actions/focusTrap";
   import { clickOutside } from "lib/actions/clickOutside";
 
   import { itemToProcessId, openedMenuId } from "../stores/clientOnlyState";
@@ -27,13 +28,23 @@
     $itemToProcessId = "";
   }}
 >
-  <div class="text-lg" use:clickOutside={() => dialog.close()}>
+  <div
+    class="text-lg"
+    use:clickOutside={() => dialog.close()}
+    use:focusTrap={{
+      focusFirstElement: true,
+      onEscape: () => dialog.close(),
+    }}
+  >
     <p class="px-4 py-2 font-bold border-b border-neutral rounded-none mb-2">Confirm Deletion</p>
 
     <div class="flex justify-around">
-      <button class="btn border border-neutral" on:click={() => dialog.close()}>Cancel</button>
+      <button data-focusindex="0" class="btn border border-neutral" on:click={() => dialog.close()}>
+        Cancel
+      </button>
 
       <button
+        data-focusindex="1"
         class="btn btn-error border border-neutral"
         on:click={() => {
           deleteItem(todoLists, listId);
