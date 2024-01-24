3e3480cfa6bc41d6530c775fced7ba3cfff5e94f
Tony Dang <tony@tonydang.blog>
2024-01-24 14:16:17 -0800

    add focus styles

--- assets/svelte/Link.svelte
+++ assets/svelte/Link.svelte
@@ -7,7 +7,10 @@
   {href}
   target={external ? "_blank" : undefined}
   rel={external ? "noopener noreferrer" : undefined}
-  class="font-semibold underline underline-offset-4 hover:no-underline"
+  class="
+    font-semibold underline underline-offset-4 hover:no-underline rounded
+    focus:outline-none focus-visible:ring ring-accent ring-offset-1 ring-offset-base-100
+  "
 >
   <slot />
 </a>
