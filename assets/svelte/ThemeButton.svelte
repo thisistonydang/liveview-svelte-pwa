5788bc8bae82c79f547233df9c544edfe459444e
Tony Dang <tony@tonydang.blog>
2024-01-24 13:36:28 -0800

    use custom focusTrap

--- assets/svelte/ThemeButton.svelte
+++ assets/svelte/ThemeButton.svelte
@@ -2,9 +2,7 @@
   import { onMount } from "svelte";
   import { scale } from "svelte/transition";
 
-  import { focusTrap } from "@skeletonlabs/skeleton";
-
-  import { onEscape } from "lib/actions/onEscape";
+  import { focusTrap } from "lib/actions/focusTrap";
   import SwatchSvgIcon from "lib/svg-icons/SwatchSvgIcon.svelte";
 
   import { openedMenuId } from "../stores/clientOnlyState";
@@ -14,7 +12,7 @@
   export let menuClass: string;
 
   const themeMenuId = "theme-menu-id";
-  let trapFocus = false;
+  let focusFirstElement = false;
 
   onMount(() => {
     const theme = JSON.parse(localStorage.getItem("theme"));
@@ -27,7 +25,7 @@
   });
 
   $: if ($openedMenuId !== themeMenuId) {
-    trapFocus = false;
+    focusFirstElement = false;
   }
 </script>
 
@@ -42,7 +40,7 @@
     on:click={() => ($openedMenuId = $openedMenuId === themeMenuId ? "" : themeMenuId)}
     on:keydown={(event) => {
       if (event.key === "Enter" || event.key === " ") {
-        trapFocus = true;
+        focusFirstElement = true;
       }
     }}
   >
@@ -52,16 +50,18 @@
   {#if $openedMenuId === themeMenuId}
     <div
       in:scale={{ duration: 100 }}
-      use:focusTrap={trapFocus}
-      use:onEscape={() => ($openedMenuId = "")}
+      use:focusTrap={{
+        focusFirstElement,
+        onEscape: () => ($openedMenuId = ""),
+      }}
       class="menu bg-base-200 border border-neutral rounded-box absolute right-0"
     >
       <p class="px-4 py-2 font-bold border-b border-neutral rounded-none mb-1.5">Theme</p>
 
       <ul class="w-28">
-        <ThemeChoiceButton theme="system" />
-        <ThemeChoiceButton theme="light" />
-        <ThemeChoiceButton theme="dark" />
+        <ThemeChoiceButton focusIndex={0} theme="system" />
+        <ThemeChoiceButton focusIndex={1} theme="light" />
+        <ThemeChoiceButton focusIndex={2} theme="dark" />
       </ul>
     </div>
   {/if}
