f1b65ff5519360d658988609330b3201c5228524
Tony Dang <tony@tonydang.blog>
2024-01-26 09:06:13 -0800

    add backdrop-blur to sticky header

--- assets/svelte/StickyHeader.svelte
+++ assets/svelte/StickyHeader.svelte
@@ -7,7 +7,7 @@
 <svelte:window bind:scrollY />
 
 <div
-  class="sticky top-0 left-0 w-full z-10 bg-base-100 border-neutral h-[68px]"
+  class="sticky top-0 left-0 w-full z-10 backdrop-blur border-neutral h-[68px]"
   class:border-b={scrollY > 0}
 >
   <div class="max-w-2xl mx-auto px-2 md:p-0 h-full flex items-center">
