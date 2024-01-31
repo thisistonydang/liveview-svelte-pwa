fbaaa6c497b54cc492b80aa0ef54e6014b18f766
Tony Dang <tony@tonydang.blog>
2024-01-31 10:43:27 -0800

    receive server_document from LiveView in Svelte

--- assets/svelte/LiveViewSocket.svelte
+++ assets/svelte/LiveViewSocket.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
-  import { liveView, serverState, sessionCount } from "../stores/liveViewSocket";
+  import { liveView, serverDocument, serverState, sessionCount } from "../stores/liveViewSocket";
   import VisibilityChangeTracker from "./VisibilityChangeTracker.svelte";
 
   // Store 'live' object from live_svelte in a Svelte store.
@@ -9,10 +9,12 @@
   $liveView = live;
 
   // Receive socket assigns from LiveView.
+  export let server_document;
   export let server_state;
   export let session_count;
 
   // Assign socket assigns to reactive stores.
+  $: $serverDocument = server_document;
   $: $serverState = server_state;
   $: $sessionCount = session_count;
 
