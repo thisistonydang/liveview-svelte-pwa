114a5cba47d393a7915a1635d44a351aa46ffea3
Tony Dang <tony@tonydang.blog>
2024-02-02 14:52:13 -0800

    update DataClearer to clear indexedDB

--- assets/svelte/DataClearer.svelte
+++ assets/svelte/DataClearer.svelte
@@ -1,13 +1,31 @@
-<script lang="ts">
-  import { onMount } from "svelte";
+<script context="module">
   import { requestAssetDeletion } from "../lib/offline-svelte";
   import config from "../../priv/static/sw.config.js";
+
+  export const indexedDBName = "ToDo";
+
+  export function clearUserData() {
+    // Clear client state.
+    localStorage.clear();
+
+    // Clear cached assets.
+    requestAssetDeletion(config.privateAssets);
+
+    // Clear indexedDB.
+    const DBDeleteRequest = window.indexedDB.deleteDatabase(indexedDBName);
+    DBDeleteRequest.onerror = () => {
+      console.error("Error deleting database.");
+    };
+  }
+</script>
+
+<script lang="ts">
+  import { onMount } from "svelte";
 
   export let live = undefined;
   live;
 
   onMount(() => {
-    localStorage.clear();
-    requestAssetDeletion(config.privateAssets);
+    clearUserData();
   });
 </script>
