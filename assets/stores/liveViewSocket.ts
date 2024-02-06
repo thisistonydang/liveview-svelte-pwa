5d925c0ecf52cbe2ef8c3fa7f5725c0d0b061a37
Tony Dang <tony@tonydang.blog>
2024-02-05 16:28:40 -0800

    add typing

--- assets/stores/liveViewSocket.ts
+++ assets/stores/liveViewSocket.ts
@@ -1,7 +1,12 @@
 import { writable } from "svelte/store";
 
+// TODO: Import type instead of defining it here if possible.
+export interface Live {
+  pushEvent: (event: string, payload?: { [key: string]: string }) => void;
+}
+
 // Store containing 'live' object from live_svelte.
-export let liveView = writable();
+export let liveView = writable<Live>();
 
 // Socket assigns from LiveView
 export let serverDocument = writable<{ event: string; document: string | null }>();
