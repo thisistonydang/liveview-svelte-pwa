0b88465226d629e624d280242cb3e21b40661f1e
Tony Dang <tony@tonydang.blog>
2024-02-06 14:00:21 -0800

    rm isConnected export

--- assets/lib/offline-svelte/index.js
+++ assets/lib/offline-svelte/index.js
@@ -1,4 +1,3 @@
-export { isConnected } from "./lib/isConnected";
 export { isSWUpdateAvailable } from "./lib/isSWUpdateAvailable";
 export { isSWUpdateConfirmed } from "./lib/isSWUpdateConfirmed";
 export { registerServiceWorker } from "./lib/registerServiceWorker";
