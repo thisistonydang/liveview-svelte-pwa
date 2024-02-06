71414184a16c422048af3d6a63a471a1dcf3d5cf
Tony Dang <tony@tonydang.blog>
2024-02-06 12:35:08 -0800

    add types

--- assets/stores/clientOnlyState.ts
+++ assets/stores/clientOnlyState.ts
@@ -1,11 +1,11 @@
 import { writable } from "svelte/store";
 
-export const isListsOpened = writable(true);
-export const isTodoOpened = writable(true);
-export const itemToProcessId = writable("");
-export const newList = writable("");
-export const newTodo = writable("");
-export const openedMenuId = writable("");
+export const isListsOpened = writable<boolean>(true);
+export const isTodoOpened = writable<boolean>(true);
+export const itemToProcessId = writable<string>("");
+export const newList = writable<string>("");
+export const newTodo = writable<string>("");
+export const openedMenuId = writable<string>("");
 
-export const selectedListId = writable("");
+export const selectedListId = writable<string>("");
 export const urlHash = writable<"" | "about" | "listId">("");
