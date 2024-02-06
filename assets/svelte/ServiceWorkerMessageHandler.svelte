065f843f710fe973e678a31e00b261507d5eb600
Tony Dang <tony@tonydang.blog>
2024-02-06 15:02:01 -0800

    mv sw message helpers

--- assets/svelte/ServiceWorkerMessageHandler.svelte
+++ assets/svelte/ServiceWorkerMessageHandler.svelte
@@ -1,3 +1,31 @@
+<script lang="ts" context="module">
+  import config from "../../priv/static/sw.config.js";
+
+  export function requestAssetCaching(assets: string[]) {
+    navigator.serviceWorker?.controller?.postMessage({
+      type: config.messageTypes.REQUEST_ASSET_CACHING,
+      payload: {
+        assets,
+      },
+    });
+  }
+
+  export function requestAssetDeletion(assets: string[]) {
+    navigator.serviceWorker?.controller?.postMessage({
+      type: config.messageTypes.REQUEST_ASSET_DELETION,
+      payload: {
+        assets,
+      },
+    });
+  }
+
+  export function requestServiceWorkerVersion() {
+    navigator.serviceWorker?.controller?.postMessage({
+      type: config.messageTypes.REQUEST_SERVICE_WORKER_VERSION,
+    });
+  }
+</script>
+
 <script lang="ts">
   import { onMount } from "svelte";
   import { serviceWorkerVersion } from "$stores/serviceWorkerVersion";
