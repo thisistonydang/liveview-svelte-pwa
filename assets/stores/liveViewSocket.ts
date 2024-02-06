4622d0aa5bf2061b4ed7b60cdcc5ecf3e9f0386e
Tony Dang <tony@tonydang.blog>
2024-02-05 16:32:40 -0800

    import type from live_svelte

--- assets/stores/liveViewSocket.ts
+++ assets/stores/liveViewSocket.ts
@@ -1,9 +1,6 @@
 import { writable } from "svelte/store";
 
-// TODO: Import type instead of defining it here if possible.
-export interface Live {
-  pushEvent: (event: string, payload?: { [key: string]: string }) => void;
-}
+import type { Live } from "node_modules/live_svelte/assets/js/live_svelte/types";
 
 // Store containing 'live' object from live_svelte.
 export let liveView = writable<Live>();
