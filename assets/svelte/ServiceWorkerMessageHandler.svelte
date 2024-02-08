de4b8db75aab462e625fb48bc6766735bedd4506
Tony Dang <tony@tonydang.blog>
2024-02-08 08:50:18 -0800

    add getAppJsAndCss helper

--- assets/svelte/ServiceWorkerMessageHandler.svelte
+++ assets/svelte/ServiceWorkerMessageHandler.svelte
@@ -1,6 +1,18 @@
 <script lang="ts" context="module">
   import config from "../../priv/static/sw.config.js";
 
+  export function getAppJsAndCss() {
+    const appJsScript: HTMLScriptElement = document.querySelector("script[phx-track-static]");
+    const appJsUrl = new URL(appJsScript.src);
+    const appJs = `${appJsUrl.pathname}${appJsUrl.search}`;
+
+    const appCssLink: HTMLLinkElement = document.querySelector("link[phx-track-static]");
+    const appCssUrl = new URL(appCssLink.href);
+    const appCss = `${appCssUrl.pathname}${appCssUrl.search}`;
+
+    return { appJs, appCss };
+  }
+
   function requestAssetCaching(assets: string[]) {
     navigator.serviceWorker?.controller?.postMessage({
       type: config.messageTypes.REQUEST_ASSET_CACHING,
@@ -53,14 +65,7 @@
   onMount(() => {
     useIsConnected({ timeout: 10000 }).then((isConnected) => {
       if (isConnected) {
-        const appJsScript: HTMLScriptElement = document.querySelector("script[phx-track-static]");
-        const appJsUrl = new URL(appJsScript.src);
-        const appJs = `${appJsUrl.pathname}${appJsUrl.search}`;
-
-        const appCssLink: HTMLLinkElement = document.querySelector("link[phx-track-static]");
-        const appCssUrl = new URL(appCssLink.href);
-        const appCss = `${appCssUrl.pathname}${appCssUrl.search}`;
-
+        const { appJs, appCss } = getAppJsAndCss();
         requestAssetCaching([...config.privateAssets, ...config.publicAssets, appJs, appCss]);
       }
     });
