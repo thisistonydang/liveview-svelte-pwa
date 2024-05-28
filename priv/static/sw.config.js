148d0f38730982540bb148bf78ea04424c400d23
Tony Dang <tony@tonydang.blog>
2024-05-27 17:04:48 -0700

    bump svelte-dnd-version

--- priv/static/sw.config.js
+++ priv/static/sw.config.js
@@ -1,5 +1,5 @@
 export default {
-  cacheName: "v2024.05.27.1",
+  cacheName: "v2024.05.27.2",
   debug: false,
   disableCache: false, // Set to true during dev to disable caching.
   messageTypes: {
