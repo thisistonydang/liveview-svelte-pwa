04da2d03db1b6e42b621898dcc5f8db36d433629
Tony Dang <tony@tonydang.blog>
2024-06-07 12:38:26 -0700

    extract assets into seprate file

---
+++ assets/lib/assets.ts
@@ -0,0 +1,22 @@
+export const privateAssets = ["/app"];
+export const publicAssets = [
+  "/favicon.ico",
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png",
+  "/apple-touch-icon.png",
+  "/browserconfig.xml",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/mstile-150x150.png",
+  "/og.png",
+  "/safari-pinned-tab.svg",
+  "/screenshot-narrow-light.png",
+  "/screenshot-narrow-dark.png",
+  "/screenshot-wide-light.png",
+  "/screenshot-wide-dark.png",
+  "/site.webmanifest",
+
+  // Note: This route should only be cached after login so that
+  // DataClearer does not run.
+  "/offline",
+];
--- priv/static/sw.js
+++ priv/static/sw.js
@@ -13,30 +13,6 @@ const config = {
     REQUEST_SKIP_WAITING: "request_skip_waiting",
     REQUEST_SERVICE_WORKER_VERSION: "request_service_worker_version",
   },
-  privateAssets: [
-    "/app", 
-  ],
-  publicAssets: [
-    "/favicon.ico",
-    "/android-chrome-192x192.png",
-    "/android-chrome-512x512.png",
-    "/apple-touch-icon.png",
-    "/browserconfig.xml",
-    "/favicon-16x16.png",
-    "/favicon-32x32.png",
-    "/mstile-150x150.png",
-    "/og.png",
-    "/safari-pinned-tab.svg",
-    "/screenshot-narrow-light.png",
-    "/screenshot-narrow-dark.png",
-    "/screenshot-wide-light.png",
-    "/screenshot-wide-dark.png",
-    "/site.webmanifest",
-
-    // Note: This route should only be cached after login so that
-    // DataClearer does not run.
-    "/offline", 
-  ]
 }
 
 const { cacheName, debug, disableCache, messageTypes } = config;
