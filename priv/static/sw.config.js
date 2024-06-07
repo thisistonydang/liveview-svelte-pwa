327a9383c2d2eee4b0d083dd9322b4f63f6b532c
Tony Dang <tony@tonydang.blog>
2024-06-07 11:14:18 -0700

    Bump sw version. Fixes detecting inaccurate connection status on Safari

--- priv/static/sw.config.js
+++ priv/static/sw.config.js
@@ -1,5 +1,5 @@
 export default {
-  cacheName: "v2024.06.05.1",
+  cacheName: "v2024.06.07.2",
   debug: false,
   disableCache: false, // Set to true during dev to disable caching.
   messageTypes: {
